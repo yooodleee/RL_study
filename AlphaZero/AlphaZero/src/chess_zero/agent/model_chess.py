8b08463927e0e5ca7cf55f1a576fd8c75d6c319c
yooodleee <accia25@naver.com>
2024-12-26 22:21:50 +0900

    update model_chess

--- AlphaZero/AlphaZero/src/chess_zero/agent/model_chess.py
+++ AlphaZero/AlphaZero/src/chess_zero/agent/model_chess.py
@@ -40,14 +40,14 @@ class ChessModel:
 
         # (batch, channels, height, width)
         x = Conv2D(
-            filters=mc.cnn_filter_num,
-            kernel_size=mc.cnn_first_filter_size,
-            padding="same",
-            data_format="channels_first",
-            use_bias=False,
-            kernel_regularizer=l2(mc.cnn_filter_num))(x)
-        x = BatchNormalization(axis=1, name="input_batchnorm")(x)
-        x = Activation("relu", name="input_relu")(x)
+            filters = mc.cnn_filter_num,
+            kernel_size = mc.cnn_first_filter_size,
+            padding = "same",
+            data_format = "channels_first",
+            use_bias = False,
+            kernel_regularizer = l2(mc.cnn_filter_num))(x)
+        x = BatchNormalization(axis = 1, name = "input_batchnorm")(x)
+        x = Activation("relu", name = "input_relu")(x)
 
         for i in range(mc.res_layer_num):
             x = self._build_residual_block(x, i + 1)
@@ -56,18 +56,71 @@ class ChessModel:
 
         # for policy output
         x = Conv2D(
-            filters=2,
-            kernel_size=1,
-            data_format="channels_first",
-            use_bias=False,
-            kernel_regularizer=l2(mc.l2_reg),
+            filters = 2,
+            kernel_size = 1,
+            data_format = "channels_first",
+            use_bias = False,
+            kernel_regularizer = l2(mc.l2_reg),
             name="policy_conv-1-2")(res_out)
-        x = BatchNormalization(axis=1, name="policy_batchnorm")(x)
-        x = Activation("relu", name="policy_relu")(x)
-        x = Flatten(name="policy_flatten")(x)
+        x = BatchNormalization(axis = 1, name = "policy_batchnorm")(x)
+        x = Activation("relu", name = "policy_relu")(x)
+        x = Flatten(name = "policy_flatten")(x)
         # no output for 'pass'
         policy_out = Dense(
             self.config.n_labels,
-            kernel_regularizer=l2(mc.l2_reg),
-            activation="softmax",
-            name="policy_out")(x)
+            kernel_regularizer = l2(mc.l2_reg),
+            activation = "softmax",
+            name = "policy_out")(x)
+        
+
+        # for value output
+        x = Conv2D(
+            filters = 4,
+            kernel_size = 1,
+            data_format="channels_first",
+            use_bias = False,
+            kernel_regularizer = l2(mc.l2_reg),
+            name = "value_conv-1-4")(res_out)
+        x = BatchNormalization(axis = 1, name = "value_batchnorm")(x)
+        x = Activation("relu", name = "value_relu")(x)
+        x = Flatten(name = "value_flatten")(x)
+        x = Dense(
+            mc.value_fc_size,
+            kernel_regularizer = l2(mc.l2_reg),
+            activation = "relu",
+            name = "value_dense")(x)
+        value_out = Dense(
+            1, kernel_regularizer=l2(mc.l2_reg), activation="tanh", name="value_out")(x)
+        
+        self.model = Model(in_x, [policy_out, value_out], name="chess_model")
+    
+    def _build_residual_block(self, x, index):
+        mc = self.config.model
+        in_x = x
+        res_name = "res" + str(index)
+        x = Conv2D(
+            filters = mc.cnn_filter_num,
+            kernel_size = mc.cnn_filter_size,
+            padding = "same",
+            data_format = "channels_first",
+            use_bias = False,
+            kernel_regularizer = l2(mc.l2_reg),
+            name = res_name + "_conv1-" + str(mc.cnn_filter_size) + "-" + str(mc.cnn_filter_num))(x)
+        x = BatchNormalization(axis = 1, name = res_name + "_batchnorm1")(x)
+        x = Activation("relu", name = res_name + "_relu1")(x)
+        x = Conv2D(
+            filters = mc.cnn_filter_num,
+            kernel_size = mc.cnn_filter_size,
+            padding = "same",
+            data_format = "channels_first",
+            use_bias = False,
+            kernel_regularizer = l2(mc.l2_reg),
+            name = res_name + "_conv2-" + str(mc.cnn_filter_size) + "-" + str(mc.cnn_filter_num))(x)
+        x = BatchNormalization(axis=1, name="res" + str(index) + "_batchnorm2")(x)
+        x = Add(name=res_name + "_add")([in_x, x])
+        x = Activation("relu", name=res_name + "_relu2")(x)
+        return x
+    
+    @staticmethod
+    def fetch_digest(weight_path):
+        ''
