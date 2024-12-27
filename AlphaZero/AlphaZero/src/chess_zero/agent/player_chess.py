5ff8374adf971d7bb1edf6f0fa786b53b7e5283f
yooodleee <accia25@naver.com>
2024-12-27 16:01:41 +0900

    update player_chess and create distributed

--- AlphaZero/AlphaZero/src/chess_zero/agent/player_chess.py
+++ AlphaZero/AlphaZero/src/chess_zero/agent/player_chess.py
@@ -122,4 +122,150 @@ class ChessPlayer:
             # assert env.whiteon != env.white_to_move
             # side to move can't be winner!
         
+        state = state_key(env)
+
+        with self.node_lock[state]:
+            if state not in self.tree:
+                leaf_p, leaf_v = self.expand_and_evaluate(env)
+                self.tree[state].p = leaf_p
+                return leaf_v
+            # returning everything from the POV of side to move
+            # assert state in self.tree
+
+            # SELECT STEP
+            action_t = self.select_action_q_and_u(env, is_root_node)
+
+            virtual_loss = self.play_config.virtual_loss
+
+            my_visit_stats = self.tree[state]
+            my_stats = my_visit_stats.a[action_t]
+
+            my_visit_stats.sum_n += virtual_loss
+            my_stats.n += virtual_loss
+            my_stats.w += -virtual_loss
+            my_stats.q = my_stats.w / my_stats.n
         
+        env.step(action_t.uci())
+        leaf_v = self.search_my_move(env)   # next move from enemy POV
+        leaf_v = -leaf_v
+
+        # BACKUP STEP
+        # on returning search path
+        # update: N, W, Q
+        with self.node_lock[state]:
+            my_visit_stats.sum_n += -virtual_loss + 1
+            my_stats.n += -virtual_loss + 1
+            my_stats.w += virtual_loss + leaf_v
+            my_stats.q = my_stats.w / my_stats.n
+        
+        return leaf_v
+    
+    def expand_and_evaluate(self, env)-> Union[np.ndarray, float]:
+        """Expand new leaf, this is called only once per state
+        this is called with state locked
+        insert P(a|s), return leaf_v
+        """
+        state_planes = env.canonical_input_planes()
+
+        leaf_p, leaf_v = self.predict(state_planes)
+        # these are canonical policy and value (i.e. side to move is "white")
+
+        if not env.white_to_move:
+            leaf_p = Config.flip_policy(leaf_p) # get it back to python-chess form
+        # np.testing.assert_array_equal(Config.flip_policy(Config.flip_policy(leaf_p)), leaf_p)
+
+        return leaf_p, leaf_v
+    
+    def predict(self, state_planes):
+        pipe = self.pipe_pool.pop()
+        pipe.send(state_planes)
+        ret = pipe.recv()
+        self.pipe_pool.append(pipe)
+        return ret
+    
+    #@profile
+    def select_action_q_and_u(self, env, is_root_node)-> chess.Move:
+        # this method is called with state locked
+        state = state_key(env)
+
+        my_visitstats = self.tree[state]
+
+        if my_visitstats.p is not None: # push p to edges
+            top_p = 1e-8
+            for mov in env.board.legal_moves:
+                mov_p = my_visitstats.p[self.move_lookup[mov]]
+                my_visitstats.a[mov].p = mov_p
+                top_p += mov_p
+            for a_s in my_visitstats.a.values():
+                a_s.p /= top_p
+            my_visitstats.p = None
+        
+        xx_ = np.sqrt(my_visitstats.sum_n + 1)  # sqrt of sum(N(s, b); for all b)
+
+        e = self.play_config.noise_eps
+        c_puct = self.play_config.c_puct
+        dir_alpha = self.play_config.dirichlet_alpha
+
+        best_s = -999
+        best_a = None
+
+        for action, a_s in my_visitstats.a.items():
+            p_ = a_s.p
+            if is_root_node:
+                p_ = (1-e) * p_ + e * np.random.dirichlet([dir_alpha])
+            b = a_s.q + c_puct * p_ * xx_ / (1 + a_s.n)
+            if b > best_s:
+                best_s = b
+                best_a = action
+        
+        return best_a
+    
+    def apply_temperature(self, policy, turn):
+        tau = np.power(self.play_config.tau_decay_rate, turn + 1)
+        if tau < 0.1:
+            tau = 0
+        if tau == 0:
+            action = np.argmax(policy)
+            ret = np.zeros(self.labels_n)
+            ret[action] = 1.0
+            return ret
+        else:
+            ret = np.power(policy, 1/tau)
+            ret /= np.sum(ret)
+            return ret
+    
+    def calc_policy(self, env):
+        """calc π(a|s0)"""
+
+        state = state_key(env)
+        my_visitstats = self.tree[state]
+        policy = np.zeros(self.labels_n)
+        for action, a_s in my_visitstats.a.items():
+            policy[self.move_lookup[action]] = a_s.n
+        
+        policy /= np.sum(policy)
+        return policy
+    
+    def sl_action(self, observation, my_action, weight=1):
+        policy = np.zeros(self.labels_n)
+
+        k = self.move_lookup[chess.Move.from_uci(my_action)]
+        policy[k] = weight
+
+        self.moves.append([observation, list(policy)])
+        return my_action
+    
+    def finish_game(self, z):
+        """
+        Params:
+            z: win=1, 
+            lose=-1, 
+            draw=0
+        """
+        for move in self.moves: # add this game winner result to all past moves.
+            move += [z]
+
+
+def state_key(env, ChessEnv)-> str:
+    fen = env.board.fen().rsplit(' ', 1)    # drop the move clock
+    return fen[0]
