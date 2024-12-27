2deadb821e6bd2141bef4b6953c9453136377179
yooodleee <accia25@naver.com>
2024-12-27 20:24:51 +0900

    create tensorflow util

---
+++ AlphaZero/AlphaZero/src/chess_zero/lib/tf_util.py
@@ -0,0 +1,20 @@
+def set_session_config(
+    per_process_gpu_memory_fraction=None,
+    allow_growth=None):
+    """
+    Params:
+        allow_growth: When necessary, reserve memory
+        (float) per_process_gpu_memory_fraction: specify GPU memory usage as 0 to 1
+    """
+
+    import tensorflow as tf
+    import keras.backend as k
+
+    config = tf.compat.v1.ConfigProto(
+        gpu_options = tf.compat.v1.GPUOptions(
+            per_process_gpu_memory_fraction = per_process_gpu_memory_fraction,
+            allow_growth = allow_growth,
+        )
+    )
+    sess = tf.compat.v1.Session(config=config)
+    k.set_session(sess)
