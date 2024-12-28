df2f2ec3e23960ec1240daa6f86fd5d15d87f632
yooodleee <accia25@naver.com>
2024-12-28 23:24:39 +0900

    create Train class

---
+++ Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/trainer.py
@@ -0,0 +1,29 @@
+import copy
+import time
+
+import numpy
+import ray
+import torch
+
+import models
+
+
+@ray.remote
+class Trainer:
+    """
+    Class which run in a dedicated thread to train a neural network 
+    and save it in the shared storage.
+    """
+
+    def __init__(self, initial_checkpoint, config):
+        self.config = config
+
+        # Fix random generator seed
+        numpy.random.seed(self.config.seed)
+        torch.manual_seed(self.config.seed)
+
+        # Initialize the network
+        self.model = models.MuZeroNetwork(self.config)
+        self.model.set_weights(copy.deepcopy(initial_checkpoint["weights"]))
+        self.model.to(torch.device("cuda" if self.config.train_on_gpu else "cpu"))
+        self.model.train()
