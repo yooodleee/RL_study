7668f10835bca84bbe537ad97a34dcc69cb992d1
yooodleee <accia25@naver.com>
2024-12-28 20:47:09 +0900

    create Node class

--- Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/self_play.py
+++ Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/self_play.py
@@ -440,3 +440,56 @@ class MCTS:
 
 
 
+class Node:
+    def __init__(self, prior):
+        self.visit_count = 0
+        self.to_play = -1
+        self.prior = prior
+        self.value_sum = 0
+        self.children = {}
+        self.hidden_state = None
+        self.reward = 0
+    
+    def expanded(self):
+        return len(self.children) > 0
+    
+    def value(self):
+        if self.visit_count == 0:
+            return 0
+        return self.value_sum / self.visit_count
+    
+    def expand(
+        self,
+        actions,
+        to_play,
+        reward,
+        policy_logits,
+        hidden_state,
+    ):
+        """
+        Expand a node using the value, reward and policy prediction obtained from the
+        neural network.
+        """
+        self.to_play = to_play
+        self.reward = reward
+        self.hidden_state = hidden_state
+
+        policy_value = torch.softmax(
+            torch.tensor([policy_logits[0][a] for a in actions]), dim=0
+        ).tolist()
+        policy = {a: policy_value[i] for i, a in enumerate(actions)}
+        for action, p in policy.items():
+            self.children[action] = Node(p)
+    
+    def add_exploration_noise(self, dirichlet_alpha, exploration_fraction):
+        """
+        At the start of each search, add dirichlet noise to the prior of the root to
+        encourage the search to explore new actions.
+        """
+        actions = list(self.children.keys())
+        noise = numpy.random.dirichlet([dirichlet_alpha] * len(actions))
+        frac = exploration_fraction
+        for a, n in zip(actions, noise):
+            self.children[a].prior = self.children[a].prior * (1 - frac) + n * frac
+
+
