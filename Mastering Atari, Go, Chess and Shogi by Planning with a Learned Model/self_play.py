d87f0d003ba4e4d14560828fbe62940ed0e83331
yooodleee <accia25@naver.com>
2024-12-28 21:59:24 +0900

    create DiagnoseModel class

--- Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/self_play.py
+++ Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/self_play.py
@@ -80,7 +80,7 @@ class SelfPlay:
                     shared_storage.set_info.remote(
                         {
                             "muzero_reward": sum(
-                                rewawrd
+                                reward
                                 for i, reward in enumerate(game_history.reward_history)
                                 if game_history.to_play_history[i -1]
                                 == self.config.muzero_player
@@ -493,7 +493,7 @@ class Node:
             self.children[a].prior = self.children[a].prior * (1 - frac) + n * frac
 
 
-class gameHistory:
+class GameHistory:
     """
     Store only usefull information of a self-play game.
     """
@@ -567,3 +567,21 @@ class gameHistory:
         return stacked_observations
 
 
+class MinMaxStats:
+    """
+    A class that holds the min-max values of the tree.
+    """
+
+    def __init__(self):
+        self.maximum = -float("inf")
+        self.minimum = float("inf")
+    
+    def update(self, value):
+        self.maximum = max(self.maximum, value)
+        self.minimum = min(self.minimum, value)
+    
+    def normalize(self, value):
+        if self.maximum > self.minimum:
+            # Normalize only when we have set the maximum and minimum values
+            return (value - self.minimum) / (self.maximum - self.minimum)
+        return value
