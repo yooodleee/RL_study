a98327cfe4f421ba31fb52299e51701c57caad85
yooodleee <accia25@naver.com>
2024-12-28 19:58:10 +0900

    definition MCTS's run method

--- Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/self_play.py
+++ Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/self_play.py
@@ -11,7 +11,8 @@ import models
 @ray.remote
 class SelfPlay:
     """Class with run in a dedicated thread to play games and
-    save them to the replay-buffer."""
+    save them to the replay-buffer.
+    """
 
     def __init__(self, initial_checkpoint, Game, config, seed):
         self.config = config
@@ -234,7 +235,8 @@ class SelfPlay:
     def select_action(node, temperature):
         """Select action according to the visit count distribution and the temperature.
         The temperature is changed dynamically with the visit_softmax_temperature function
-        in the config."""
+        in the config.
+        """
 
         visit_counts = numpy.array(
             [child.visit_count for child in node.children.values()], dtype="int32"
@@ -256,3 +258,115 @@ class SelfPlay:
 
 
 # Game independent.
+class MCTS:
+    """Core Monte Carlo Tree Search(MCTS) algorithm.
+    To decide on an action, run N simulations, always starting at the root of
+    the search tree and traversing the tree according to the UCB formula until
+    reach a leaf node.
+    """
+
+    def __init__(self, config):
+        self.config = config
+    
+    def run(
+        self,
+        model,
+        observation,
+        legal_actions,
+        to_play,
+        add_exploration_noise,
+        override_root_with=None,
+    ):
+        """At the root of the search tree, use the representation function to obtain a
+        hidden state given the current observation.
+        Then run a Monte Carlo Tree Search(MCTS) using only action sequences and the model
+        learned by the network.
+        """
+        if override_root_with:
+            root = override_root_with
+            root_predicted_value = None
+        else:
+            root = Node(0)
+            observation = (
+                torch.tensor(observation)
+                .float()
+                .unsqueeze(0)
+                .to(next(model.parameters()).device)
+            )
+            (
+                root_predicted_value,
+                reward,
+                policy_logits,
+                hidden_state,
+            ) = model.initial_inference(observation)
+            root_predicted_value = models.support_to_scalar(
+                root_predicted_value, self.config.support_size
+            ).item()
+            reward = models.support_to_scalar(reward, self.config.support_size).item()
+            assert (
+                legal_actions
+            ), f"Legal actions should not be an empty array. Got {legal_actions}."
+            assert set(legal_actions).issubset(
+                set(self.config.action_space)
+            ), "Legal actions should be a subset of the action space."
+            root.expand(
+                legal_actions,
+                to_play,
+                reward,
+                policy_logits,
+                hidden_state,
+            )
+        
+        if add_exploration_noise:
+            root.add_exploration_noise(
+                dirichlet_alpha = self.config.root_dirichlet_alpha,
+                exploration_fraction = self.config.root_exploration_fraction,
+            )
+        
+        min_max_stats = MinMaxStats()
+
+        max_tree_depth = 0
+        for _ in range(self.config.num_simulations):
+            virtual_to_play = to_play
+            node = root
+            search_path = [node]
+            current_tree_depth = 0
+
+            while node.expanded():
+                current_tree_depth += 1
+                action, node = self.select_child(node, min_max_stats)
+                search_path.append(node)
+
+                # Players play turn by turn
+                if virtual_to_play + 1 < len(self.config.players):
+                    virtual_to_play = self.config.players[virtual_to_play + 1]
+                else:
+                    virtual_to_play = self.config.players[0]
+            
+            # Inside the search tree we use the dynamics function to obtain the next hidden
+            # state given an action and the previous hidden state
+            parent = search_path[-2]
+            value, reward, policy_logits, hidden_state = model.recurrent_inference(
+                parent.hidden_state,
+                torch.tensor([[action]]).to(parent.hidden_state.device),
+            )
+            value = models.support_to_scalar(value, self.config.support_size).item()
+            reward = models.support_to_scalar(reward, self.config.support_size).item()
+            node.expand(
+                self.config.action_space,
+                virtual_to_play,
+                reward,
+                policy_logits,
+                hidden_state,
+            )
+
+            self.backpropagate(search_path, value, virtual_to_play, min_max_stats)
+
+            max_tree_depth = max(max_tree_depth, current_tree_depth)
+        
+        extra_info = {
+            "max_tree_depth": max_tree_depth,
+            "root_predicted_value": root_predicted_value,
+        }
+        return root, extra_info
+    
