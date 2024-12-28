4fbf0da61126875145c5027ac0a4a3e1d418da32
yooodleee <accia25@naver.com>
2024-12-28 17:03:56 +0900

    fix conv3x3's error

--- Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/models.py
+++ Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/models.py
@@ -203,7 +203,7 @@ class MuZeroFullyConnectedNetwork(AbstractNetwork):
 ############## ResNet ##############
 
 
-def conv3x3(in_channels, out_channels, ,stride=1):
+def conv3x3(in_channels, out_channels, stride=1):
     return torch.nn.Conv2d(
         in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False
     )
