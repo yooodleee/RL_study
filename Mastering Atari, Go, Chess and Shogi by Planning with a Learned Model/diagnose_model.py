5e9e855c0f18ece60008458f42ce29d27d302125
yooodleee <accia25@naver.com>
2024-12-28 23:07:41 +0900

    update djagnose model using plotlib to diagnose models

--- Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/diagnose_model.py
+++ Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/diagnose_model.py
@@ -201,3 +201,179 @@ class Trajectoryinfo:
 
     def __init__(self, title, config):
         self.title = title + ": "
+        self.config = config
+        self.action_history = []
+        self.reward_history = []
+        self.prior_policies = []
+        self.policies_after_planning = []
+        # Not implemented, need to store them in every nodes of the mcts
+        self.prior_values = []
+        self.values_after_planning = [[numpy.nan] * len(self.config.action_space)]
+        self.prior_root_value = []
+        self.root_value_after_planning = []
+        self.prior_rewards = [[numpy.nan] * len(self.config.action_space)]
+        self.mcts_depth = []
+    
+    def store_info(
+        self,
+        root,
+        mcts_info,
+        action,
+        reward,
+        new_prior_root_value=None,
+    ):
+        if action is not None:
+            self.action_history.append(action)
+        if reward is not None:
+            self.reward_history.append(reward)
+        self.prior_policies.append(
+            [
+                root.children[action].prior
+                if action in root.children.keys()
+                else numpy.nan
+                for action in self.config.action_space
+            ]
+        )
+        self.policies_after_planning.append(
+            [
+                root.children[action].visit_count / self.config.num_simulations
+                if action in root.children.keys()
+                else numpy.nan
+                for action in self.config.action_space
+            ]
+        )
+        self.values_after_planning.append(
+            [
+                root.children[action].value()
+                if action in root.children.keys()
+                else numpy.nan
+                for action in self.config.action_space
+            ]
+        )
+        self.prior_root_value.append(
+            mcts_info["root_predicted_value"]
+            if not new_prior_root_value
+            else new_prior_root_value
+        )
+        self.root_value_after_planning.append(root.value())
+        self.prior_rewards.append(
+            [
+                root.children[action].reward
+                if action in root.children.keys()
+                else numpy.nan
+                for action in self.config.action_space
+            ]
+        )
+        self.mcts_depth.append(mcts_info["max_tree_depth"])
+    
+    def plot_trajectory(self):
+        name = "Prior policies"
+        print(name, self.prior_policies, "\n")
+        plt.figure(self.title + name)
+        ax = seaborn.heatmap(
+            self.prior_policies,
+            mask=numpy.isnan(self.prior_policies),
+            annot=True,
+        )
+        ax.set(xlabel="Action", ylabel="Timestep")
+        ax.set_title(name)
+
+        name = "Policies after planning"
+        print(name, self.policies_after_planning, "\n")
+        plt.figure(self.title + name)
+        ax = seaborn.heatmap(
+            self.policies_after_planning,
+            mask=numpy.isnan(self.policies_after_planning),
+            annot=True
+        )
+        ax.set(xlabel="Action", ylabel="Timestep")
+        ax.set_title(name)
+
+        if 0 < len(self.action_history):
+            name = "Action history"
+            print(name, self.action_history, "\n")
+            plt.figure(self.title + name)
+            # ax  = seaborn.lineplot(x=list(range(len(self.action_history))), y=self.action_history)
+            ax = seaborn.heatmap(
+                numpy.transpose([self.action_history]),
+                mask=numpy.isnan(numpy.transpose([self.action_history])),
+                xticklabels=False,
+                annot=True,
+            )
+            ax.set(ylabel="Timestep")
+            ax.set_title(name)
+        
+        name = "Values after planning"
+        print(name, self.values_after_planning, "\n")
+        plt.figure(self.title + name)
+        ax = seaborn.heatmap(
+            self.values_after_planning,
+            mask=numpy.isnan(self.values_after_planning),
+            annot=True,
+        )
+        ax.set(xlabel="Action", ylabel="Timestep")
+        ax.set_title(name)
+
+        name = "Prior root value"
+        print(name, self.prior_root_value)
+        plt.figure(self.title + name)
+        # ax = seaborn.lineplot(x=list(range(len(self.prior_root_value))), y=self.prior_root_value)
+        ax = seaborn.heatmap(
+            numpy.transpose([self.prior_root_value]),
+            mask=numpy.isnan(numpy.transpose([self.prior_root_value])),
+            xticklabels=False,
+            annot=True,
+        )
+        ax.set(ylabel="Timestep")
+        ax.set_title(name)
+
+        name = "Root value after planning"
+        print(name, self.root_value_after_planning, "\n")
+        plt.figure(self.title + name)
+        # ax = seaborn.lineplot(x=list(range(len(self.root_value_after_planning))), y=self.root_value_after_planning)
+        ax = seaborn.heatmap(
+            numpy.transpose([self.root_value_after_planning]),
+            mask=numpy.isnan(numpy.transpose([self.root_value_after_planning])),
+            xticklabels=False,
+            annot=True,
+        )
+        ax.set(ylabel="Timestep")
+        ax.set_title(name)
+
+        name = "Prior rewards"
+        print(name, self.prior_rewards, "\n")
+        plt.figure(self.title + name)
+        ax = seaborn.heatmap(
+            self.prior_rewards, mask=numpy.isnan(self.prior_rewards), annot=True
+        )
+        ax.set(xlabel="Action", ylabel="Timestep")
+        ax.set_title(name)
+
+        if 0 < len(self.reward_history):
+            name = "Reward history"
+            print(name, self.reward_history, "\n")
+            plt.figure(self.title + name)
+            # ax = seaborn.lineplot(x=list(range(len(self.reward_history))), y=self.reward_history)
+            ax = seaborn.heatmap(
+                numpy.transpose([self.reward_history]),
+                mask=numpy.isnan(numpy.transpose([self.reward_history])),
+                xticklabels=False,
+                annot=True,
+            )
+            ax.set(ylabel="Timestep")
+            ax.set_title(name)
+        
+        name = "MCTS depth"
+        print(name, self.mcts_depth, "\n")
+        plt.figure(self.title + name)
+        # ax = seaborn.lineplot(x=list(range(len(self.mcts_depth))), y=self.mcts_depth)
+        ax = seaborn.heatmap(
+            numpy.transpose([self.mcts_depth]),
+            mask=numpy.isnan(numpy.transpose([self.mcts_depth])),
+            xticklabels=False,
+            annot=True,
+        )
+        ax.set(ylabel="Timestep")
+        ax.set_title(name)
+
+        plt.show(block=False)
