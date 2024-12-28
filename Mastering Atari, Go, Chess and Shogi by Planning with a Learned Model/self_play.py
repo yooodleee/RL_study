66a8284c7c715c5d65ab4e8549e24c73406af7a2
yooodleee <accia25@naver.com>
2024-12-28 20:58:58 +0900

    create gameHistory class

--- Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/self_play.py
+++ Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/self_play.py
@@ -493,3 +493,77 @@ class Node:
             self.children[a].prior = self.children[a].prior * (1 - frac) + n * frac
 
 
+class gameHistory:
+    """
+    Store only usefull information of a self-play game.
+    """
+
+    def __init__(self):
+        self.observation_history = []
+        self.action_history = []
+        self.reward_history = []
+        self.to_play_history = []
+        self.child_values = []
+        self.root_values = []
+        self.reanalysed_predicted_root_values = None
+        # For PER
+        self.priorities = None
+        self.game_priority = None
+    
+    def store_search_statistics(self, root, action_space):
+        # Turn visit count from root into a policy
+        if root is not None:
+            sum_visit = sum(child.visit_count for child in root.children.values())
+            self.child_visits.append(
+                [
+                    root.children[a].visit_count / sum_visit
+                    if a in root.children
+                    else 0
+                    for a in action_space
+                ]
+            )
+
+            self.root_values.append(root.value())
+        else:
+            self.root_values.append(None)
+    
+    def get_stacked_observations(
+        self, index, num_stacked_observations, action_space_size
+    ):
+        """
+        Generate a new observation with the observation at the index position
+        and num_stacked_observations past observations and actions stacked.
+        """
+        # Convert to positive index
+        index = index % len(self.observation_history)
+
+        stacked_observations = self.observation_history[index].copy()
+        for past_observation_index in reversed(
+            range(index - num_stacked_observations, index)
+        ):
+            if 0 <= past_observation_index:
+                previous_observation = numpy.concatenate(
+                    (
+                        self.observation_history[past_observation_index],
+                        [
+                            numpy.ones_like(stacked_observations[0])
+                            * self.action_history[past_observation_index + 1]
+                            / action_space_size
+                        ],
+                    )
+                )
+            else:
+                previous_observation = numpy.concatenate(
+                    (
+                        numpy.zeros_like(self.observation_history[index]),
+                        [numpy.zeros_like(stacked_observations[0])],
+                    )
+                )
+            
+            stacked_observations = numpy.concatenate(
+                (stacked_observations, previous_observation)
+            )
+        
+        return stacked_observations
+
+
