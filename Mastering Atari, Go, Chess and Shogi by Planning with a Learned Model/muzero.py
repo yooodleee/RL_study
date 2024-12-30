96a2feb465aff9848ba28187d120d2dc53cd3b2f
yooodleee <accia25@naver.com>
2024-12-30 18:42:01 +0900

    end hyperparameter_search method

--- Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/muzero.py
+++ Mastering Atari, Go, Chess and Shogi by Planning with a Learned Model/muzero.py
@@ -511,4 +511,71 @@ def hyperparameter_search(
     optimizer = nevergrad.optimizers.OnePlusOne(
         parametrization=parameterization, budget=budget
     )
+
+    running_experiments = []
+    best_training = None
+    try:
+        # Launch initial experiments
+        for i in range(parallel_experiments):
+            if 0 < budget:
+                param = optimizer.ask()
+                print(f"Launching new experiment: {param.value}")
+                muzero = MuZero(game_name, param.value, parallel_experiments)
+                muzero.param = param
+                muzero.train(False)
+                running_experiments.append(muzero)
+                budget -= 1
+            
+            while 0 < budget or any(running_experiments):
+                for i, experiment in enumerate(running_experiments):
+                    if experiment and experiment.config.training_steps <= ray.get(
+                        experiment.shared_storage_worker.get_info.remote("training_step")
+                    ):
+                        experiment.terminate_workers()
+                        result = experiment.text(False, num_tests=num_tests)
+                        if not best_training or best_training["result"] < result:
+                            best_training = {
+                                "result": result,
+                                "config": experiment.config,
+                                "checkpoint": experiment.checkpoint,
+                            }
+                        print(f"Parameters: {experiment.param.value}")
+                        print(f"Result: {result}")
+                        optimizer.tell(experiment.param, -result)
+
+                        if 0 < budget:
+                            param = optimizer.ask()
+                            print(f"Launching new experiment: {param.value}")
+                            muzero = MuZero(game_name, param.value, parallel_experiments)
+                            muzero.param = param
+                            muzero.train(False)
+                            running_experiments[i] = muzero
+                            budget -= 1
+                        else:
+                            running_experiments[i] = None
+        
+    except KeyboardInterrupt:
+        for experiment in running_experiments:
+            if isinstance(experiment, MuZero):
+                experiment.terminate_workers()
     
+    recommendation = optimizer.provide_recommendation()
+    print("Best hyperparameters:")
+    print(recommendation.value)
+    if best_training:
+        # Save best training weights (but it's not the recommended weights)
+        best_training["config"].results_path.mkdir(parents=True, exist_ok=True)
+        torch.save(
+            best_training["checkpoint"],
+            best_training["config"].results_path / "model.checkpoint",
+        )
+        # Save the recommended hyperparameters
+        text_file = open(
+            best_training["config"].results_path / "best_parameters.txt",
+            "w"
+        )
+        text_file.write(str(recommendation.value))
+        text_file.close()
+    return recommendation.value
+
+
