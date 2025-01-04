8e5f135e7b6745e9efa8d77328477426fb340bfd
yooodleee <accia25@naver.com>
2025-01-04 19:47:58 +0900

    adds boolean key loss_info

--- DDRL/gym_env.py
+++ DDRL/gym_env.py
@@ -123,3 +123,48 @@ class StickyAction(gym.Wrapper):
         return self.env.reset(**kwargs)
 
 
+class LifeLoss(gym.Wrapper):
+    """
+    Adds boolean key 'loss_life' into the info dict, but only reset on true 
+        game over.
+    """
+
+    def __init__(self, env):
+        gym.Wrapper.__init__(self, env)
+        self.lives = 0
+        self.was_real_terminated = True
+    
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        self.was_real_terminated = done
+
+        # Check current lives, make loss of life terminal,
+        # then update lives to handle bonus lives
+        lives = self.env.unwrapped.ale.lives()
+
+        if lives < self.lives and lives > 0:
+            # for Qbert somtimes we stay in lives == 0 condition for a few frames
+            # so it's important to keep lives > 0, so that we only reset once
+            # the environment advertises done.
+            info['loss_life'] = True
+        else:
+            info['loss_life'] = False
+        
+        self.lives = lives
+        return obs, reward, done, info
+    
+    def reset(self, **kwargs):
+        """
+        Reset only when lives are exhausted.
+        This way all states are still reachable even though lives are episodic,
+            and the learner need not know about any of this behind-the-scenes.
+        """
+        if self.was_real_terminated:
+            obs = self.env.reset(**kwargs)
+        else:
+            # no-op step to advance from terminal/lost life state
+            obs, _, _, _ = self.env.step(0)
+        self.lives = self.env.unwrapped.lives()
+        return obs
+    
+
