e2d47e1c2b6192bc579347e0f6274fd77790ff65
yooodleee <accia25@naver.com>
2025-01-04 20:12:55 +0900

    create LazyFrames object

--- DDRL/gym_env.py
+++ DDRL/gym_env.py
@@ -294,3 +294,48 @@ class FrameStack(gym.Wrapper):
         return LazyFrames(list(self.frames))
 
 
+class LazyFrames(object):
+    """
+    This object ensures that common frames between the observations are
+        only stored once. It exists purely to optimize memory usage which 
+        can be huge for DQN's 1M frames replay buffers.
+    This object should only be converted to numpy array before being passed
+        to the model. You'd not believe how complex the previous solution was.
+    """
+
+    def __init__(self, frames):
+        self.dtype = frames[0].dtype
+        self.shape = (
+            frames[0].shape[0],
+            frames[0].shape[1],
+            len(frames)
+        )
+        self._frames = frames
+        self._out = None
+
+    def _force(self):
+        if self._out is None:
+            self._out = np.concatenate(self._frames, axis=-1)
+            self._frames = None
+        return self._out
+    
+    def __array__(self, dtype=None):
+        out = self._force()
+        if dtype is not None:
+            out = out.astype(dtype)
+        return out
+    
+    def __len__(self):
+        return len(self._force())
+    
+    def __getitem__(self, i):
+        return self._force()[i]
+    
+    def count(self):
+        frames = self._force()
+        return frames.shape[frames.ndim - 1]
+    
+    def frame(self, i):
+        return self._force()[..., i]
+
+
