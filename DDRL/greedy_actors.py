69294fd6ef972075e093439493054da24c4a8394
yooodleee <accia25@naver.com>
2025-01-04 17:20:42 +0900

    create NGU e-greedy actor

--- DDRL/greedy_actors.py
+++ DDRL/greedy_actors.py
@@ -254,3 +254,143 @@ class R2d2EpsilonGreedyActor(EpsilonGreedyActor):
         self._lstm_state = self._network.get_initial_hidden_state(batch_size=1)
 
 
+class NguEpsilonGreedyActor(EpsilonGreedyActor):
+    """
+    NGU e-greedy actor.
+    """
+
+    def __init__(
+        self,
+        network: torch.nn.Module,
+        embedding_network: torch.nn.Module,
+        rnd_target_network: torch.nn.Module,
+        rnd_predictor_network: torch.nn.Module,
+        episodic_memory_capacity: int,
+        num_neibors: int,
+        cluster_distance: float,
+        kernel_epsilon: float,
+        max_similarity: float,
+        exploration_epsilon: float,
+        random_state: np.random.RandomState,    # pylint: disable=no-member
+        device: torch.device,
+    ):
+        super().__init__(
+            network,
+            exploration_epsilon,
+            random_state,
+            device,
+            'NGU-greedy',
+        )
+
+        self._policy_index = 0
+        self._policy_beta = 0
+
+        # Episodic intrinsic bonus module
+        self._episodic_module = EpisodicBonusModule(
+            embedding_network=embedding_network,
+            device=device,
+            capacity=episodic_memory_capacity,
+            num_neibors=num_neibors,
+            kernel_epsilon=kernel_epsilon,
+            cluster_distance=cluster_distance,
+            max_similarity=max_similarity,
+        )
+
+        # Lifelong intrinsic bonus module
+        self._lifelong_module = RndLifeLongBonusModule(
+            target_network=rnd_target_network,
+            predictor_network=rnd_predictor_network,
+            device=device,
+            discount=0.99,
+        )
+
+        self._last_action = None
+        self._episodic_bonus_t = None
+        self._lifelong_bonus_t = None
+        self._lstm_state = self._network.get_initial_hidden_state(batch_size=1)
+
+    @torch.no_grad()
+    def step(
+        self, timestep: types_lib.TimeStep
+    )-> types_lib.Action:
+        """
+        Give current timestep, return best action.
+        """
+        a_t = self._select_action(timestep)
+        s_t = torch.from_numpy(
+            timestep.observation[None, ...]
+        ).to(device=self._device, dtype=torch.float32)
+
+        # Compute lifelong intrinsic bonus
+        self._lifelong_bonus_t = self._lifelong_module.compute_bonus(s_t)
+
+        # Compute episodic intrinsic bonus
+        self._episodic_bonus_t = self._episodic_module.compute_bonus(s_t)
+        return a_t
+    
+    @torch.no_grad()
+    def _select_action(
+        self, timestep: types_lib.TimeStep
+    )-> types_lib.Action:
+        """
+        Samples action from eps-greedy wrt Q-values at given state.
+        """
+        s_t = torch.tensor(
+            timestep.observation[None, ...]
+        ).to(device=self._device, dtype=torch.float32)
+        a_tm1 = torch.tensor(
+            self._last_action
+        ).to(device=self._device, dtype=torch.float64)
+        ext_r_t = torch.tensor(
+            timestep.reward
+        ).to(device=self._device, dtype=torch.float32)
+        int_r_t = torch.tensor(
+            self.intrinsic_reward
+        ).to(device=self._device, dtype=torch.float32)
+        policy_index = torch.tensor(
+            self._policy_index
+        ).to(device=self._device, dtype=torch.int64)
+        hidden_s = tuple(
+            s.to(device=self._device) for s in self._lstm_state
+        )
+
+        pi_output = self._network(
+            NguNetworkInputs(
+                s_t=s_t[None, ...], # [T, B, state_shape]
+                a_tm1=a_tm1[None, ...], # [T, B]
+                ext_r_t=ext_r_t[None, ...], # [T, B]
+                int_r_t=int_r_t[None, ...], # [T, B]
+                policy_index=policy_index[None, ...],   # [T, B]
+                hidden_s=hidden_s,
+            )
+        )
+
+        q_t = pi_output.q_values
+        self._lstm_state = pi_output.hidden_s
+
+        a_t = apply_egreedy_policy(
+            q_t, self._exploration_epsilon, self._random_state
+        )
+        self._last_action = a_t
+
+        return a_t
+    
+    def reset(self)-> None:
+        """
+        Reset hidden state to zeros at new episodes.
+        """
+        self._last_action = 0   # Initialize a_tm1 to 0.
+        self._episodic_bonus_t = 0.0
+        self._lifelong_bonus_t = 0.0
+        self._lstm_state = self._network.get_initial_hidden_state(batch_size=1)
+        self._episodic_module.reset()
+    
+    @property
+    def intrinsic_reward(self)-> float:
+        """
+        Returns intrinsic reward for last state s_tm1.
+        """
+        # Equation 1 of the NGU paper.
+        return self._episodic_bonus_t * min(max(self._lifelong_bonus_t, 1.0), 5.0)
+
+
