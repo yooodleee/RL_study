6c9dc2c4a1eb1620f5a220bee4d76aad31b6a009
yooodleee <accia25@naver.com>
2025-01-04 19:52:45 +0900

    return only every skip-th frame

--- DDRL/gym_env.py
+++ DDRL/gym_env.py
@@ -168,3 +168,43 @@ class LifeLoss(gym.Wrapper):
         return obs
     
 
+class MaxAndSkip(gym.Wrapper):
+    """
+    Return only every `skip`-th frame
+    """
+
+    def __init__(self, env, skip=4):
+        gym.Wrapper.__init__(self, env)
+        # Most recent raw observations (for max pooling across time steps)
+        self._obs_buffer = np.zeros(
+            (2,) + env.observation_space.shape, dtype=np.uint8
+        )
+        self._skip = skip
+    
+    def step(self, action):
+        """
+        Repeat action, sum reward, and max over last observations.
+        """
+        total_reward = 0.0
+        done = None
+
+        for i in range(self._skip):
+            obs, reward, done, info = self.env.step(action)
+            if i == self._skip - 2:
+                self._obs_buffer[0] = obs
+            if i == self._skip - 1:
+                self._obs_buffer[1] = obs
+            total_reward += reward
+            if done:
+                break
+        
+        # Note that the observation on the done=True frame
+        # doesn't matter
+        max_frame = self._obs_buffer.max(axis=0)
+
+        return max_frame, total_reward, done, info
+    
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
+
+
