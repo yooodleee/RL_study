bed0d23821644de0c0c4286ee67a7203e8c6a621
yooodleee <accia25@naver.com>
2025-01-05 20:49:25 +0900

    RunningMeanstd class for RND networks

--- DDRL/normalizer.py
+++ DDRL/normalizer.py
@@ -21,7 +21,10 @@ class TorchRnningMeanStd:
             shape, dtype=torch.float32, device=self.device
         )
         self.count = 0
-    
+
+        self.deltas = []
+        self.min_size = 10
+
     @torch.no_grad()
     def update(self, x):
         x = x.to(self.device)
@@ -40,7 +43,7 @@ class TorchRnningMeanStd:
     
     @torch.no_grad()
     def update_single(self, x):
-        self.detlas.append(x)
+        self.deltas.append(x)
 
         if len(self.deltas) >= self.min_size:
             batched_x = torch.concat(self.deltas, dim=0)
@@ -55,3 +58,41 @@ class TorchRnningMeanStd:
         )
 
 
+class RunningMeanStd:
+    """
+    For RND networks.
+    """
+
+    def __init__(self, shape=()):
+        self.mean = np.zeros(shape, 'float32')
+        self.var = np.ones(shape, 'float32')
+        self.count = 0
+
+        self.deltas = []
+        self.min_size = 10
+    
+    def update(self, x):
+        batch_mean = np.mean(x, axis=0)
+        batch_var = np.var(x, axis=0)
+
+        # Update count and moments
+        n = x.shape[0]
+        self.count += n
+        delta = batch_mean - self.mean
+        self.mean += delta * n / self.count
+        m_a = self.var * (self.count - n)
+        m_b = batch_var * n
+        M2 = m_a + m_b + np.square(delta) * self.count * n / self.count
+        self.var = M2 / self.count
+
+    def update_single(self, x):
+        self.deltas.append(x)
+
+        if len(self.deltas) >= self.min_size:
+            batched_x = np.stack(self.deltas, axis=0)
+            self.update(batched_x)
+
+            del self.deltas[:]
+    
+    def normalize(self, x):
+        return (x - self.mean) / np.sqrt(self.var + 1e-8)
