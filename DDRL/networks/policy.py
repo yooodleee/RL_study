a1f0ea8a3209c771c56174d5e05b6b228907adc2
yooodleee <accia25@naver.com>
2025-01-08 20:10:48 +0900

    create ImpalaActorCriticNetworkInputs class

--- DDRL/networks/policy.py
+++ DDRL/networks/policy.py
@@ -34,3 +34,11 @@ class ImpalaActorCriticNetworkOutputs(NamedTuple):
     hidden_s: torch.Tensor
 
 
+class ImpalaActorCriticNetworkInputs(NamedTuple):
+    s_t: torch.Tensor
+    a_tm1: torch.Tensor
+    r_t: torch.Tensor   # reward for (s_tm1, a_tm1), but received at current timestep.
+    done: torch.Tensor
+    hidden_s: Optional[Tuple[torch.Tensor]]
+
+
