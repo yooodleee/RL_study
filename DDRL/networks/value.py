89bcbb2eef4bd7a5f38aa76e492a4baa024a3601
yooodleee <accia25@naver.com>
2025-01-09 14:38:43 +0900

    fix and Create RnnDqnNetworkInputs

--- DDRL/networks/value.py
+++ DDRL/networks/value.py
@@ -16,22 +16,31 @@ from networks import common
 
 
 class DqnNetworkOutputs(NamedTuple):
-    q_values = torch.Tensor
+    q_values: torch.Tensor
 
 
 class C51NetworkOutputs(NamedTuple):
-    q_values = torch.Tensor
-    q_logits = torch.Tensor # Use logits and log_softmax() when calculate loss to avoid log() on zero cause NaN
+    q_values: torch.Tensor
+    q_logits: torch.Tensor # Use logits and log_softmax() when calculate loss to avoid log() on zero cause NaN
 
 
 class QRDqnNetworkOutputs(NamedTuple):
-    q_values = torch.Tensor
-    q_dist = torch.Tensor
+    q_values: torch.Tensor
+    q_dist: torch.Tensor
 
 
 class IqnNetworkOutputs(NamedTuple):
-    q_values = torch.Tensor
-    q_dist = torch.Tensor
-    taus = torch.Tensor
+    q_values: torch.Tensor
+    q_dist: torch.Tensor
+    taus: torch.Tensor
+
+
+class RnnDqnNetworkInputs(NamedTuple):
+    s_t: torch.Tensor
+    a_tm1: torch.Tensor
+    r_t: torch.Tensor   # reward for (s_tm1, a_tm1), but received at current timestep.
+    hidden_s: Optional[
+        Tuple[torch.Tensor, torch.Tensor]
+    ]
 
 
