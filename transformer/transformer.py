48692251ede60f115379dbcb5feea6f9264ceb53
yooodleee <accia25@naver.com>
2025-01-09 15:45:40 +0900

    selfattention

---
+++ transformer/transformer.py
@@ -0,0 +1,111 @@
+"""
+A from scratch implementation of Transformer network
+
+Original source code from:
+    https://github.com/aladdinpersson/Machine-Learning-Collection/tree/master
+
+Raw implementation of Transformer architecture.    
+"""
+
+import torch
+import torch.nn as nn
+
+
+class SelfAttention(nn.Module):
+
+    def __init__(
+        self, embed_size, heads
+    ):
+        super(SelfAttention, self).__init__()
+        
+        self.embed_size = embed_size
+        self.heads = heads
+        self.head_dim = embed_size // heads
+
+        assert (
+            self.head_dim * heads == embed_size
+        ), "Embedding size needs to be divisible by heads"
+
+        self.values = nn.Linear(
+            embed_size, embed_size
+        )
+        self.keys = nn.Linear(
+            embed_size, embed_size
+        )
+        self.queries = nn.Linear(
+            embed_size, embed_size
+        )
+        self.fc_out = nn.Linear(
+            embed_size, embed_size
+        )
+    
+    def forward(
+        self,
+        values,
+        keys,
+        query,
+        mask,
+    ):
+        """
+        Get number training examples.
+        """
+        N = query.shape[0]
+
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+
+        values = self.values(values)    # (N, value_len, embed_size)
+        keys = self.keys(keys)  # (N, key_len, embed_size)
+        queries = self.queries(query)   # (N, query_len, embed_size)
+
+        # Split the embedding into self.heads different pices.
+        values = values.reshape(
+            N, value_len, self.heads, self.head_dim
+        )
+        keys = keys.reshape(
+            N, key_len, self.heads, self.head_dim
+        )
+        queries = queries.reshape(
+            N, query_len, self.heads, self.head_dim
+        )
+
+        # Einsum does matrix mult. for query * Keys for each training example
+        # with every other training example, don't be confused by eninsum
+        # it's just how doing matrix multiplication & bmm
+
+        energy = torch.einsum(
+            "nqhd,nkhd->nhqk", [queries, keys]
+        )
+        # queries shape: (N, query_len, heads, heads_dim)
+        # keys shape: (N, key_len, heads, heads_dim)
+        # energy: (N, heads, query_len, key_len)
+
+        # Mask padded indicies so their weights become 0
+        if mask is not None:
+            energy = energy.masked_fill(
+                mask == 0, float("-1e20")
+            )
+        
+        # Normalize energy values similary to seq2seq + attention
+        # so that they sum to 1. Also divide by scaling factor for
+        # better stability.
+        attention = torch.softmax(
+            energy / (self.embed_size ** (1 / 2)), dim=3
+        )
+        # attention shape: (N, heads, query_len, key_len)
+
+        out = torch.einsum(
+            "nhql,nlhd->nqhd",
+            [attention, values]
+        ).reshape(N, query_len, self.heads * self.head_dim)
+        # attention shape: (N, heads, query_len, key_len)
+        # values shape : (N, value_len, headds, heads_dim)
+        # out after matrix multiply: (N, query_len, heads, head_dim),
+        # then reshape and flatten the last two dimensions.
+
+        out = self.fc_out(out)
+        # Linear layer doesn't modify the shape, final shape will be
+        # (N, query_len, embed_size)
+
+        return out
+
+
