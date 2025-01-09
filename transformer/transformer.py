ad1cd1c97f10d5bb34aa51704e9900502207e225
yooodleee <accia25@naver.com>
2025-01-09 15:55:46 +0900

    Encoder

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -149,3 +149,73 @@ class TransformerBlock(nn.Module):
         return out
 
 
+class Encoder(nn.Module):
+    """
+    Encoder architecture.
+
+    Args:
+        src_vocab_size:
+        embed_size:
+        num_layers:
+        heads:
+        device:
+        forward_expansion:
+        dropout:
+        max_length:
+    """
+
+    def __init__(
+        self,
+        src_vocal_size,
+        embed_size,
+        num_layers,
+        heads,
+        device,
+        forward_expansion,
+        dropout,
+        max_length,
+    ):
+        super(Encoder, self).__init__()
+
+        self.embed_size = embed_size
+        self.device = device
+        self.word_embedding = nn.Embedding(
+            src_vocal_size, embed_size
+        )
+        self.position_embedding = nn.Embedding(
+            max_length, embed_size
+        )
+
+        self.layers = nn.ModuleList(
+            [
+                TransformerBlock(
+                    embed_size,
+                    heads,
+                    dropout,
+                    forward_expansion=forward_expansion,
+                )
+                for _ in range(num_layers)
+            ]
+        )
+
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(
+        self, x, mask
+    ):
+        N, seq_length = x.shape
+        positions = torch.arange(
+            0, seq_length
+        ).expand(N, seq_length).to(self.device)
+        out = self.dropout(
+            (self.word_embedding(x) + self.position_embedding(positions))
+        )
+
+        # In the Encoder the query, key, value are all the same, it's in the
+        # decoder this will change. This might look a bit odd in this case.
+        for layer in self.layers:
+            out = layer(out, out, out, out)
+        
+        return out
+
+
