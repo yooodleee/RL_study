4e7c376cabb6c01ba6e8a4ef3b839137675a73af
yooodleee <accia25@naver.com>
2025-01-12 01:08:20 +0900

    fix code style

--- DDRL/r2d2/eval_agent.py
+++ DDRL/r2d2/eval_agent.py
@@ -6,7 +6,7 @@ from absl import app, flags, logging
 import numpy as np
 import torch
 
-# pylint: disable-import-error
+
 from networks.value import R2d2DqnMlpNet, R2d2DqnConvNet
 from checkpoint import PyTorchCheckpoint
 from .. import main_loop, gym_env, greedy_actors
@@ -19,37 +19,59 @@ flags.DEFINE_string(
     'Both Classic control tasks name like CartPole-v1, LunarLander-v2, MountainCar-v0, Arcboot-v1. and Atari game like Pong, Breakout.'
 )
 flags.DEFINE_integer(
-    'environment_height', 84, 'Environment frame screen height, for atari only.'
+    'environment_height', 
+    84, 
+    'Environment frame screen height, for atari only.',
 )
 flags.DEFINE_integer(
-    'environment_width', 84, 'Environment frame screen width, for atari only.'
+    'environment_width',
+    84,
+    'Environment frame screen width, for atari only.',
 )
 flags.DEFINE_integer(
-    'environment_frame_skip', 4, 'Number of frames to skip, for atari only.'
+    'environment_frame_skip',
+    4, 
+    'Number of frames to skip, for atari only.',
 )
 flags.DEFINE_integer(
-    'environment_frame_stack', 1, 'Number of frames to stack.'
+    'environment_frame_stack', 
+    1, 
+    'Number of frames to stack.',
 )
 flags.DEFINE_float(
-    'eval_exploration_epsilon', 0.01, 'Fixed exploration rate in e-greedy policy for evaluation.'
+    'eval_exploration_epsilon', 
+    0.01, 
+    'Fixed exploration rate in e-greedy policy for evaluation.',
 )
 flags.DEFINE_integer(
-    'num_iterations', 1, 'Number of evaluation iterations to run.'
+    'num_iterations', 
+    1, 
+    'Number of evaluation iterations to run.',
 )
 flags.DEFINE_integer(
-    'num_eval_steps', int(2e4), 'Number of evaluation steps (environment steps or frames) to run per iteraion.'
+    'num_eval_steps', 
+    int(2e4), 
+    'Number of evaluation steps (environment steps or frames) to run per iteraion.',
 )
 flags.DEFINE_integer(
-    'max_episode_steps', 58000, 'Maximum steps (before frame skip) per episode, for atari only.'
+    'max_episode_steps', 
+    58000, 
+    'Maximum steps (before frame skip) per episode, for atari only.',
 )
 flags.DEFINE_integer(
-    'seed', 1, 'Runtime seed.'
+    'seed', 
+    1, 
+    'Runtime seed.',
 )
 flags.DEFINE_bool(
-    'use_tensorboard', True, 'Use Tensorboard to monitor statistics, default on.'
+    'use_tensorboard', 
+    True, 
+    'Use Tensorboard to monitor statistics, default on.',
 )
 flags.DEFINE_string(
-    'load_checkpoint_file', '', 'Load a specific checkpoint file.'
+    'load_checkpoint_file', 
+    '', 
+    'Load a specific checkpoint file.',
 )
 flags.DEFINE_string(
     'record_video_dir',
@@ -58,7 +80,8 @@ flags.DEFINE_string(
 )
 
 flags.register_validator(
-    'environment_frame_stack', lambda x: x == 1
+    'environment_frame_stack', 
+    lambda x: x == 1,
 )
 
 
@@ -77,51 +100,63 @@ def main(argv):
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
     
-    random_state = np.random.RandomState(FLAGS.seed)    # pylint: disable=no-member
+    random_state = np.random.RandomState(FLAGS.seed)    
 
     # Create evaluation environments
     if FLAGS.environment_name in gym_env.CLASSIC_ENV_NAMES:
         eval_env = gym_env.create_classic_environment(
-            env_name=FLAGS.environment_name, seed=random_state.randint(1, 2**10)
+            env_name=FLAGS.environment_name, 
+            seed=random_state.randint(1, 2**10),
         )
         state_dim = eval_env.observation_space.shape[0]
         action_dim = eval_env.action_space.n
-        network = R2d2DqnConvNet(state_dim=state_dim, action_dim=action_dim)
+        network = R2d2DqnConvNet(
+            state_dim=state_dim, 
+            action_dim=action_dim,
+        )
     else:
         eval_env = gym_env.create_atari_environment(
-            env_name = FLAGS.environment_name,
-            frame_height = FLAGS.environment_height,
-            frame_width = FLAGS.environment_width,
-            frame_skip = FLAGS.environment_frame_skip,
-            frame_stack = FLAGS.envrionment_frame_stack,
-            max_episode_steps = FLAGS.max_episode_steps,
-            seed = random_state.randint(1, 2**10),
-            noop_max = 30,
-            terminal_on_life_loss = False,
-            sticky_action = False,
-            clip_reward = False,
+            env_name=FLAGS.environment_name,
+            frame_height=FLAGS.environment_height,
+            frame_width=FLAGS.environment_width,
+            frame_skip=FLAGS.environment_frame_skip,
+            frame_stack=FLAGS.envrionment_frame_stack,
+            max_episode_steps=FLAGS.max_episode_steps,
+            seed=random_state.randint(1, 2**10),
+            noop_max=30,
+            terminal_on_life_loss=False,
+            sticky_action=False,
+            clip_rewar=False,
         )
         state_dim = (
-            FLAGS.environment_frame_stack, FLAGS.environment_height, FLAGS.environment_width
+            FLAGS.environment_frame_stack, 
+            FLAGS.environment_height, 
+            FLAGS.environment_width,
         )
         action_dim = eval_env.action_space.n
-        network = R2d2DqnConvNet(state_dim=state_dim, action_dim=action_dim)
+        network = R2d2DqnConvNet(
+            state_dim=state_dim, 
+            action_dim=action_dim,
+        )
     
     logging.info(
-        'Environment: %s', FLAGS.environment_name
+        'Environment: %s', 
+        FLAGS.environment_name,
     )
     logging.info(
-        'Action spec: %s', action_dim
+        'Action spec: %s', 
+        action_dim,
     )
     logging.info(
-        'Observation spec: %s', state_dim
+        'Observation spec: %s', 
+        state_dim,
     )
 
     # Setup checkpoint and load model weights from checkpoint.
     checkpoint = PyTorchCheckpoint(
-        environment_name = FLAGS.environment_name,
-        agent_name = 'R2D2',
-        restore_only = True,
+        environment_name=FLAGS.environment_name,
+        agent_name='R2D2',
+        restore_only=True,
     )
     checkpoint.register_pair(('network', network))
 
@@ -132,20 +167,20 @@ def main(argv):
 
     # Create evaluation agent instance
     eval_agent = greedy_actors.R2d2EpsilonGreedyActor(
-        network = network,
-        exploration_epsilon = FLAGS.eval_exploration_epsilon,
-        random_state = random_state,
-        device = runtime_device,
+        network=network,
+        exploration_epsilon=FLAGS.eval_exploration_epsilon,
+        random_state=random_state,
+        device=runtime_device,
     )
 
     # Run test N iterations.
     main_loop.run_evaluation_iterations(
-        num_iterations = FLAGS.num_iterations,
-        num_eval_steps = FLAGS.num_eval_steps,
-        eval_agent = eval_agent,
-        eval_env = eval_env,
-        use_tensorboard = FLAGS.use_tensorboard,
-        recording_video_dir = FLAGS.recording_video_dir,   
+        num_iterations=FLAGS.num_iterations,
+        num_eval_steps=FLAGS.num_eval_steps,
+        eval_agent=eval_agent,
+        eval_env=eval_env,
+        use_tensorboard=FLAGS.use_tensorboard,
+        recording_video_dir=FLAGS.recording_video_dir,   
     )
 
 
