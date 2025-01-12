6b4585c1c83323ca95ec9a0e2b0389c4af46c4f9
yooodleee <accia25@naver.com>
2025-01-12 17:50:46 +0900

    fix code style

--- DDRL/types.py
+++ DDRL/types.py
@@ -9,8 +9,8 @@ from typing import (
     Mapping,
     Iterable,
     Optional,
-    Any,
-)
+    Any)
+
 import numpy as np
 
 
@@ -25,15 +25,15 @@ class TimeStep(NamedTuple):
     """
 
     observation: Optional[np.ndarray]
-    reward: Optional[float] # reward from the environment, could be clipped or scaled
-    done: Optional[bool]    # termination mark of a episode, could also be loss-of-life for Atari
+    # reward from the environment, could be clipped or scaled
+    reward: Optional[float] 
+    # termination mark of a episode, could also be loss-of-life for Atari
+    done: Optional[bool]    
     first: Optional[bool]   # first step of an episode
-    info: Optional[
-        Mapping[Text, Any]
-    ]   # Info directory which contains non-clipped/unscaled reward and other information,
-        # only used by the tracker
-
-
+    # Info directory which contains non-clipped/unscaled reward and other 
+    # information, only used by the tracker
+    info: Optional[Mapping[Text, Any]]   
+    
 
 class Agent(abc.ABC):
     """
@@ -44,23 +44,22 @@ class Agent(abc.ABC):
     step_t: int # runtime steps
 
     @abc.abstractmethod
-    def step(
-        self, timestep: TimeStep
-    )-> Action:
+    def step(self, timestep: TimeStep) -> Action:
         """
         Selects action given timestep and potentially learns.
         """
     
     @abc.abstractmethod
-    def reset(self)-> None:
+    def reset(self) -> None:
         """
-        Resets the agent's episodic state such as frame stack and action repeat.
+        Resets the agent's episodic state such as frame stack and action 
+            repeat.
         This method should be called at the beginning of every episode.
         """
     
     @property
     @abc.abstractmethod
-    def statistics(self)-> Mapping[Text, float]:
+    def statistics(self) -> Mapping[Text, float]:
         """
         Returns current agent statistics as a dictionary.
         """
@@ -75,29 +74,30 @@ class Learner(abc.ABC):
     step_t: int # learner steps
 
     @abc.abstractmethod
-    def step(self)-> Iterable[Mapping[Text, float]]:
+    def step(self) -> Iterable[Mapping[Text, float]]:
         """
         Increment learner step, and potentially do a update when called.
 
         Yields:
-            learner statistics if network parameters update occurred, otherwise None.
+            learner statistics if network parameters update occurred, otherwise 
+            None.
         """
     
     @abc.abstractmethod
-    def reset(self)-> None:
+    def reset(self) -> None:
         """
         Should be called at the beginning of every iteration.
         """
 
     @abc.abstractmethod
-    def received_item_from_queue(self, imte: Any)-> None:
+    def received_item_from_queue(self, imte: Any) -> None:
         """
         Received item send by actors through multiprocessing queue.
         """
 
     @property
     @abc.abstractmethod
-    def statistics(self)-> Mapping[Text, float]:
+    def statistics(self) -> Mapping[Text, float]:
         """
         Returns current agent statistics as a dictionary.
         """
