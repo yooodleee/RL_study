6daddafd7dcbc0ee715a00649e3a3db832f4a774
yooodleee <accia25@naver.com>
2025-01-12 16:13:51 +0900

    fix code style

--- DDRL/log.py
+++ DDRL/log.py
@@ -11,8 +11,8 @@ class CsvWriter:
     A logging object writing to a CSV file.
 
     Each `write()` takes a `OrderDict`, creating one column in the CSV file for
-        each dictionary key on the first call. Successive calls to `write()` must
-        contain the same dictionary keys.
+        each dictionary key on the first call. Successive calls to `write()` 
+        must contain the same dictionary keys.
     """
 
     def __init__(self, fname: str):
@@ -32,8 +32,7 @@ class CsvWriter:
         self._fieldnames = None
 
     def write(
-        self, values: collections.OrderedDict
-    )-> None:
+            self, values: collections.OrderedDict) -> None:
         """
         Appends given values as new row to CSV file.
         """
@@ -42,23 +41,22 @@ class CsvWriter:
         
         if self._fieldnames is None:
             self._fieldnames = values.keys()
-        # Open a file in 'append' mode, so we can continue logging safely to the
-        # same file after e.g. restarting from a checkpoint.
+        # Open a file in 'append' mode, so we can continue logging safely to 
+        # the same file after e.g. restarting from a checkpoint.
         with open(
             self._fname, 'a', encoding='utf-8'
         ) as file_:
-            # Always use same fieldnames to create writer, this way a consistency
-            # check is performed automatically on each write.
-            writer = csv.DictWriter(
-                file_, fieldnames=self._fieldnames
-            )
+            # Always use same fieldnames to create writer, this way a 
+            # consistency check is performed automatically on each write.
+            writer = csv.DictWriter(file_, fieldnames=self._fieldnames)
+
             # Write a header if this is the very first write.
             if not self._header_written:
                 writer.writeheader()
                 self._header_written = True
             writer.writerow(values)
 
-    def close(self)-> None:
+    def close(self) -> None:
         """
         Closes the 'CsvWriter'.
         """
