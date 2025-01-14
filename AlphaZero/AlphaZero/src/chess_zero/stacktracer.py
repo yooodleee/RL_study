ca0c9efd2860727ba5d56542417b88b5fbefc011
yooodleee <accia25@naver.com>
2025-01-14 20:35:49 +0900

    fix code style

--- AlphaZero/AlphaZero/src/chess_zero/stacktracer.py
+++ AlphaZero/AlphaZero/src/chess_zero/stacktracer.py
@@ -24,15 +24,15 @@ def stacktraces():
     for threadId, stack in sys._current_frames().items():
         code.append("\n# ThreadID: %s" % threadId)
         for filename, lineno, name, line in traceback.extract_stack(stack):
-            code.append('File: "%s", line %d, in %s' % (filename, lineno, name))
+            code.append('File: "%s", line %d, in %s' \
+                        % (filename, lineno, name))
             if line:
                 code.append(" %s" % (line.strip()))
     
-    return highlight("\n".join(code), PythonLexer(), HtmlFormatter(
-        full=False,
-        # style="native",
-        noclasses=True,
-    ))
+    return highlight("\n".join(code), PythonLexer(), 
+                     HtmlFormatter(full=False,
+                                    # style="native",
+                                    noclasses=True))
 
 
 # This part was made by naglzs
