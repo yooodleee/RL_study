9204f97e8da74fc76fe92ece8f64fde6d0c4a456
yooodleee <accia25@naver.com>
2025-01-14 20:11:52 +0900

    fix code style

--- AlphaZero/AlphaZero/src/chess_zero/worker/evaluate.py
+++ AlphaZero/AlphaZero/src/chess_zero/worker/evaluate.py
@@ -9,8 +9,10 @@ from chess_zero.agent.model_chess import ChessModel
 from chess_zero.agent.player_chess import ChessPlayer
 from config import Config
 from chess_zero.env.chess_env import ChessEnv, Winner
-from chess_zero.lib.data_helper import get_next_generation_model_dirs, pretty_print
-from chess_zero.lib.model_helper import save_as_best_model, load_best_model_weight
+from chess_zero.lib.data_helper import (get_next_generation_model_dirs,
+                                         pretty_print)
+from chess_zero.lib.model_helper import (save_as_best_model,
+                                          load_best_model_weight)
 
 logger = getLogger(__name__)
 
@@ -27,8 +29,10 @@ class EvaluateWorker:
         self.current_model = self.load_current_model()
         self.m = Manager()
         self.cur_pipes = self.m.list([
-            self.current_model.get_pipes(self.play_config.search_threads) \
-            for _ in range(self.play_config.max_processes)])
+                            self.current_model.\
+                                get_pipes(self.play_config.search_threads) \
+                                    for _ in range(\
+                                        self.play_config.max_processes)])
     
     def start(self):
         while True:
@@ -47,10 +51,14 @@ class EvaluateWorker:
             for _ in range(self.play_config.max_processes)])
         
         futures = []
-        with ProcessPoolExecutor(max_workers=self.play_config.max_processes) as executor:
+        with ProcessPoolExecutor(max_workers=self.play_config.max_processes) \
+            as executor:
             for game_idx in range(self.config.eval.game_num):
-                fut = executor.submit(
-                    play_game, self.config, cur=self.cur_pipes, ng=ng_pipes, current_white=(game_idx % 2 == 0))
+                fut = executor.submit(play_game, 
+                                      self.config, 
+                                      cur=self.cur_pipes, 
+                                      ng=ng_pipes, 
+                                      current_white=(game_idx % 2 == 0))
             
             results = []
             for fut in as_completed(futures):
@@ -60,7 +68,8 @@ class EvaluateWorker:
                 win_rate = sum(results) / len(results)
                 game_idx = len(results)
                 logger.debug(
-                    f"game {game_idx:3}: ng_score={ng_score:.1f} as {'block' if current_white else 'white'} "
+                    f"game {game_idx:3}: ng_score={ng_score:.1f} as 
+                    {'block' if current_white else 'white'} "
                     f"{'by resign ' if env.resigned else '          '}"
                     f"win_rate={win_rate * 100:5.1f}% "
                     f"{env.board.fen().split(' ')[0]}")
@@ -70,12 +79,16 @@ class EvaluateWorker:
                     colors = reversed(colors)
                 pretty_print(env, colors)
 
-                if len(results) - sum(results) >= self.config.eval.game_num * (1 - self.config.eval.replace_rate):
-                    logger.debug(f"lose count reach {results.count(0)} so give up challenge")
+                if len(results) - sum(results) >= self.config.eval.game_num \
+                    * (1 - self.config.eval.replace_rate):
+                    logger.debug(f"lose count reach {results.count(0)} so give
+                                  up challenge")
                     return False
                 
-                if sum(results) >= self.config.eval.game_num * self.config.eval.replace_rate:
-                    logger.debug(f"win count reach {results.count(1)} so change best model")
+                if sum(results) >= self.config.eval.game_num \
+                    * self.config.eval.replace_rate:
+                    logger.debug(f"win count reach {results.count(1)} so 
+                                 change best model")
                     return True
         
         win_rate = sum(results) / len(results)
@@ -84,11 +97,14 @@ class EvaluateWorker:
     
     def move_model(self, model_dir):
         rc = self.config.resource
-        # config_path = os.path.join(model_dir, rc.next_generation_model_config_filename)
-        # weight_path = os.path.join(model_dir, rc.next_generation_model_weight_filename)
+        # config_path = os.path.join(model_dir, 
+        #               rc.next_generation_model_config_filename)
+        # weight_path = os.path.join(model_dir, 
+        #               rc.next_generation_model_weight_filename)
         # os.remove(config_path)
         # os.remove(weight_path)
-        new_dir = os.path.join(rc.next_generation_model_dir, "copies", model_dir.name)
+        new_dir = os.path.join(rc.next_generation_model_dir, 
+                               "copies", model_dir.name)
         os.rename(model_dir, new_dir)
     
     def load_current_model(self):
@@ -104,24 +120,28 @@ class EvaluateWorker:
                 break
             logger.info("There is no next generation model to evaluate")
             sleep(60)
-        model_dir = dirs[-1] if self.config.eval.evaluate_latest_first else dirs[0]
-        config_path = os.path.join(model_dir, rc.next_generation_model_config_filename)
-        weight_path = os.path.join(model_dir, rc.next_generation_model_weight_filename)
+        model_dir = dirs[-1] if self.config.eval.evaluate_latest_first \
+                    else dirs[0]
+        config_path = os.path.join(model_dir, 
+                                   rc.next_generation_model_config_filename)
+        weight_path = os.path.join(model_dir, 
+                                   rc.next_generation_model_weight_filename)
         model = ChessModel(self.config)
         model.load(config_path, weight_path)
         return model, model_dir
 
 
-def play_game(
-    config, cur, ng, current_white: bool
-)-> Union[float, ChessEnv, bool]:
+def play_game(config, cur, 
+              ng, current_white: bool) -> Union[float, ChessEnv, bool]:
     
     cur_pipes = cur.pop()
     ng_pipes = ng.pop()
     env = ChessEnv().reset()
 
-    current_player = ChessPlayer(config, pipes=cur_pipes, play_config=config.eval.play_config)
-    ng_player = ChessPlayer(config, pipes=ng_pipes, play_config=config.eval.play_config)
+    current_player = ChessPlayer(config, pipes=cur_pipes, 
+                                 play_config=config.eval.play_config)
+    ng_player = ChessPlayer(config, pipes=ng_pipes, 
+                            play_config=config.eval.play_config)
 
     if current_white:
         white, black = current_player, ng_player
