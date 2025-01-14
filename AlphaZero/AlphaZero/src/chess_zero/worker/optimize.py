84b90778dc9164e29d5707b4f44bbc03aa0543db
yooodleee <accia25@naver.com>
2025-01-14 20:22:59 +0900

    fix code style

--- AlphaZero/AlphaZero/src/chess_zero/worker/optimize.py
+++ AlphaZero/AlphaZero/src/chess_zero/worker/optimize.py
@@ -10,8 +10,12 @@ import numpy as np
 
 from chess_zero.agent.model_chess import ChessModel
 from config import Config
-from chess_zero.env.chess_env import canon_input_planes, is_black_turn, testeval
-from chess_zero.lib.data_helper import get_game_data_filenames, read_game_data_from_file, get_next_generation_model_dirs
+from chess_zero.env.chess_env import (canon_input_planes, 
+                                      is_black_turn, 
+                                      testeval)
+from chess_zero.lib.data_helper import (get_game_data_filenames, 
+                                        read_game_data_from_file, 
+                                        get_next_generation_model_dirs)
 from chess_zero.lib.model_helper import load_best_model_weight
 
 from keras.optimizers import Adam
@@ -34,7 +38,8 @@ class OptimizeWorker:
         # this should just be a ring buffer i.e. queue of length 500,000 in AZ
         self.loaded_data = deque(maxlen=self.config.trainer.dataset_size)
         self.dataset = deque(), deque(), deque()
-        self.executor = ProcessLookupError(max_workers=config.trainer.cleaning_processes)
+        self.executor = ProcessLookupError(max_workers=\
+                                           config.trainer.cleaning_processes)
     
     def start(self):
         self.model = self.load_model()
@@ -49,15 +54,17 @@ class OptimizeWorker:
         
         while True:
             self.fill_queue()
-            # if self.dataset_size < self.config.trainer.min_data_size_to_learn:
-            #       logger.info(d"dataset_size={self.dataset_size} is less than {self.config.trainer.min_data_to_learn}")
+            # if self.dataset_size<self.config.trainer.min_data_size_to_learn:
+            #       logger.info(d"dataset_size={self.dataset_size} is less 
+            #       than {self.config.trainer.min_data_to_learn}")
             #       sleep(60)
             #       self.fill_queue()
             #       continue
             # self.update_learning_rate(total_steps)
             steps = self.train_epoch(self.config.trainer.epoch_to_checkpoint)
             total_steps += steps
-            # if last_save_step + self.config.trainer.save_model_steps < total_steps:
+            # if last_save_step + self.config.trainer.save_model_steps 
+            # < total_steps:
             self.save_current_model()
             last_save_step = total_steps
             a,b,c = self.dataset
@@ -65,49 +72,56 @@ class OptimizeWorker:
                 a.popleft()
                 b.popleft()
                 c.popleft()
-            # if last_load_data_step + self.config.trainer.load_data_steps < total_steps:
+            # if last_load_data_step + self.config.trainer.load_data_steps 
+            # < total_steps:
             #       self.fill_queue()
             #       last_load_data_step = total_steps
     
     def train_epoch(self, epochs):
         tc = self.config.trainer
         state_ary, policy_ary, value_ary = self.collect_all_loaded_data()
-        tensorboard_cb = TensorBoard(
-            log_dir = "./logs", batch_size = tc.batch_size, histogram_freq = 1)
+        tensorboard_cb = TensorBoard(log_dir="./logs", 
+                                     batch_size=tc.batch_size, 
+                                     histogram_freq=1)
         
-        self.model.model.fit(
-            state_ary,
-            [policy_ary, value_ary],
-            batch_size = tc.batch_size,
-            epochs = epochs,
-            shuffle = True,
-            validation_split = 0.02,
-            callbacks = [tensorboard_cb])
+        self.model.model.fit(state_ary,
+                             [policy_ary, value_ary],
+                             batch_size=tc.batch_size,
+                             epochs=epochs,
+                             shuffle=True,
+                             validation_split=0.02,
+                             callbacks=[tensorboard_cb])
         
         steps = (state_ary.shape[0] // tc.batch_size) * epochs
         return steps
     
     def compile_model(self):
         opt = Adam()    # SGD(lr=2e-1, momentum=0.9)    # Adam better?
-        losses = ['categorical_crossentropy', 'mean_squared_error'] # avoid overfit for supervised
-        self.model.model.compile(optimizer=opt, loss=losses, loss_weights=self.config.trainer.loss_weights)
+        # avoid overfit for supervised
+        losses = ['categorical_crossentropy', 'mean_squared_error'] 
+        self.model.model.compile(optimizer=opt, 
+                                 loss=losses, 
+                                 loss_weights=\
+                                    self.config.trainer.loss_weights)
     
     def save_current_model(self):
         rc = self.config.resource
         model_id = datetime.now().strftime("%Y%m%d-%H%M%S.%f")
-        model_dir = os.path.join(
-            rc.next_generation_model_dir, rc.next_generation_model_dirname_tmpl % model_id)
+        model_dir = os.path.join(rc.next_generation_model_dir, 
+                                 rc.next_generation_model_dirname_tmpl \
+                                    % model_id)
         
         os.makedirs(model_dir, exist_ok=True)
-        config_path = os.path.join(model_dir, rc.next_generation_model_config_filename)
-        weight_path = os.path.join(model_dir, rc.next_generation_model_weight_filename)
+        config_path = os.path.join(model_dir, 
+                                   rc.next_generation_model_config_filename)
+        weight_path = os.path.join(model_dir, 
+                                   rc.next_generation_model_weight_filename)
         self.model.save(config_path, weight_path)
     
     def fill_queue(self):
         futures = deque()
         with ProcessPoolExecutor(
-            max_workers=self.config.trainer.cleaning_processes
-        ) as executor:
+            max_workers=self.config.trainer.cleaning_processes) as executor:
             for _ in range(self.config.trainer.cleaning_processes):
                 if len(self.filenames) == 0:
                     break
@@ -115,13 +129,15 @@ class OptimizeWorker:
                 logger.debug(f"loading data from {filename}")
                 futures.append(executor.submit(load_data_from_file, filename))
             
-            while futures and len(self.dataset[0]) < self.config.trainer.dataset_size:
+            while futures and len(self.dataset[0]) \
+                < self.config.trainer.dataset_size:
                 for x, y in zip(self.dataset, futures.popleft().result()):
                     x.extend(y)
                 if len(self.filenames) > 0:
                     filename = self.filenames.popleft()
                     logger.debug(f"loading data from {filename}")
-                    futures.append(executor.submit(load_data_from_file(), filename))
+                    futures.append(executor.submit(load_data_from_file(), 
+                                                   filename))
     
     def collect_all_loaded_data(self):
         state_ary, policy_ary, value_ary = self.dataset
@@ -143,8 +159,10 @@ class OptimizeWorker:
         else:
             latest_dir = dirs[-1]
             logger.debug("loading latest model")
-            config_path = os.path.join(latest_dir, rc.next_generation_model_config_filename)
-            weight_path = os.path.join(latest_dir, rc.next_generation_model_weight_filename)
+            config_path = os.path.join(latest_dir, 
+                                       rc.next_generation_model_config_filename)
+            weight_path = os.path.join(latest_dir, 
+                                       rc.next_generation_model_weight_filename)
             model.load(config_path, weight_path)
         return model
     
@@ -176,11 +194,17 @@ def convert_to_cheating_data(data):
         # assert state_planes.shape == (18, 8, 8) # print(state_planes.shape)
 
         move_number = int(state_fen.split(' ')[5])
-        value_certainty = min(5, move_number) / 5   # reduces the noise of the opening...train faster
-        sl_value = value * value_certainty + testeval(state_fen, False) * (1 - value_certainty)
+        # reduces the noise of the opening...train faster
+        value_certainty = min(5, move_number) / 5   
+        sl_value = value \
+                    * value_certainty \
+                    + testeval(state_fen, False) \
+                    * (1 - value_certainty)
 
         state_list.append(state_planes)
         policy_list.append(policy)
         value_list.append(sl_value)
     
-    return np.asarray(state_list, dtype=np.float32), np.asarray(policy_list, dtype=np.float32), np.asarray(value_list, np.float32)
+    return np.asarray(state_list, dtype=np.float32), \
+            np.asarray(policy_list, dtype=np.float32), \
+            np.asarray(value_list, np.float32)
