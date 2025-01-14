577bb5c7375ca618dc13f4583aca2971f1b36280
yooodleee <accia25@naver.com>
2025-01-14 20:05:08 +0900

    fix code style

--- AlphaZero/AlphaZero/src/chess_zero/lib/tf_util.py
+++ AlphaZero/AlphaZero/src/chess_zero/lib/tf_util.py
@@ -4,17 +4,17 @@ def set_session_config(
     """
     Params:
         allow_growth: When necessary, reserve memory
-        (float) per_process_gpu_memory_fraction: specify GPU memory usage as 0 to 1
+        (float) per_process_gpu_memory_fraction: specify GPU memory usage as 
+            0 to 1
     """
 
     import tensorflow as tf
     import keras.backend as k
 
     config = tf.compat.v1.ConfigProto(
-        gpu_options = tf.compat.v1.GPUOptions(
-            per_process_gpu_memory_fraction = per_process_gpu_memory_fraction,
-            allow_growth = allow_growth,
-        )
-    )
+                gpu_options = tf.compat.v1.GPUOptions(
+                                per_process_gpu_memory_fraction = \
+                                    per_process_gpu_memory_fraction,
+                                allow_growth = allow_growth))
     sess = tf.compat.v1.Session(config=config)
     k.set_session(sess)
