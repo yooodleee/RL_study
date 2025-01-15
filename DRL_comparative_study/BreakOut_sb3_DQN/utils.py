ca61a153d67b247e45b2af61b25d79b255fc873f
yooodleee <accia25@naver.com>
2025-01-15 14:25:10 +0900

    fix code style

--- DRL_comparative_study/BreakOut_sb3_DQN/utils.py
+++ DRL_comparative_study/BreakOut_sb3_DQN/utils.py
@@ -8,102 +8,133 @@ import wandb
 from stable_baselines3.common.vec_env import DummyVecEnv
 
 '''
-The RewardLogger wrapper is used to log the rewards of each episode to wandb
-It makes sure that the rewards of each episode are stored in a list and that the current spisode reward is reset
+The RewardLogger wrapper is used to log the rewards of each episode to wandb.
+It makes sure that the rewards of each episode are stored in a list and that 
+    the current spisode reward is reset
 '''
 class RewardLogger(gym.Wrapper):
     def __init__(self, env):
         super(RewardLogger, self).__init__(env)
         # Store the reward of each episode
-        self.episode_rewards=[]
+        self.episode_rewards = []
         # Store the current episode reward
-        self.current_episode_reward=0
+        self.current_episode_reward = 0
     
-    # The step function is called every time the agent takes an action in the environment
+    # The step function is called every time the agent takes 
+    # an action in the environment
     def step(self, action):
         # Call the step function of the environment and store the results
-        obs, reward, done, truncated, info=self.env.step(action)
+        obs, reward, done, truncated, info = self.env.step(action)
         # Update the current episode reeward
         self.current_episode_reward += reward
-        # If the episode is done, store the episode reward and reset the current episode reward
+        # If the episode is done, store the episode reward and reset the 
+        # current episode reward
         if done:
             self.episode_rewards.append(self.current_episode_reward)
-            self.current_episode_reward=0
+            self.current_episode_reward = 0
         # Return the results as in a normal step function
         return obs, reward, done, truncated, info
     
-    # The reset function is called every time the environment is reset (at the beginning of each episode)
+    # The reset function is called every time the environment 
+    # is reset (at the beginning of each episode)
     def reset(self, **kwargs):
         return self.env.reset(**kwargs)
     
-    # The get_episode_rewards function returns the rewards of each episode
+    # The get_episode_rewards function returns the rewards of 
+    # each episode
     def get_episode_rewards(self):
         return self.episode_rewards
 
 
 '''
-The CusomWandbCallback is a callback* that logs the mean reward of the last 100 episodes to wandb.
-A callback is a function that is called at the end of each episode to perform some action,
-in this case, the action is logging the mean reward of the last 100 episodes to wandb.
+The CusomWandbCallback is a callback* that logs the mean reward 
+    of the last 100 episodes to wandb.
+A callback is a function that is called at the end of each episode 
+    to perform some action, in this case, the action is logging 
+    the mean reward of the last 100 episodes to wandb.
 '''
 class CustomWandbCallback(BaseCallback):
     def __init__(self, check_freq, save_path, verbose=1):
         super(CustomWandbCallback, self).__init__(verbose)
         # Define the frequnecy at which the callback is called
-        self.check_freq=check_freq
+        self.check_freq = check_freq
         # Define the path where the best model will be saved
-        self.save_path=save_path
+        self.save_path = save_path
         # Define the best mean reward as -inf
-        self.best_mean_reward= -np.inf
+        self.best_mean_reward = -np.inf
     
-    def _on_step(self)-> bool:
+    def _on_step(self) -> bool:
         '''
         The _on_step function is called at the end of each episode.
-        It returns True if the callback should be called again, and False otherwise.
-        To do this, it checks if the number of calls to the callback is a multiple of the check_freq.
-        If it is, it computes the mean reward of the last 100 episodes and logs it to wandb.
-        It also saves the model if the mean reward is greater than the best mean reward.
+        It returns True if the callback should be called again, and 
+            False otherwise.
+        To do this, it checks if the number of calls to the callback is 
+            a multiple of the check_freq.
+        If it is, it computes the mean reward of the last 100 episodes 
+            and logs it to wandb.
+        It also saves the model if the mean reward is greater than 
+            the best mean reward.
         '''
-        # Check if the number of calls to the callback is a multiple of the check_freq
+        # Check if the number of calls to the callback is a multiple 
+        # of the check_freq
         if self.n_calls % self.check_freq == 0:
-            # Gather rewards from all environments, by all environents we mean all the vectorized environment, usually there is only 1 environment in the vectorized environment
-            all_rewards=[]
-            for env in self.training_env.envs:  # self.training_env is the vectorized environment
-                logger_env=env.envs[0] if isinstance(env, DummyVecEnv) else env # env.envs[0] is the AtariWrapper which wraps the environment correctly
+            # Gather rewards from all environments, 
+            # by all environents we mean all the vectorized environment, 
+            # usually there is only 1 environment 
+            # in the vectorized environment
+            all_rewards = []
+            # self.training_env is the vectorized environment
+            for env in self.training_env.envs:  
+                # env.envs[0] is the AtariWrapper which wraps 
+                # the environment correctly
+                logger_env = env.envs[0] \
+                    if isinstance(env, DummyVecEnv) else env 
                 # Check if the logger_env is the RewardLogger wrapper
                 if isinstance(logger_env, RewardLogger):
-                    # If it is, get the rewards of each episode and store them in all_rewards
-                    all_rewards.extend(logger_env.get_episode_rewards())    # extend is used to add the elements of a list to another list
+                    # If it is, get the rewards of each episode 
+                    # and store them in all_rewards
 
-            # If there are rewards in all_rewards, compute the mean rewards of the last 100 episodes and log it to wnadb
+                    # extend is used to add the elements of a list 
+                    # to another list
+                    all_rewards.extend(logger_env.get_episode_rewards())    
+
+            # If there are rewards in all_rewards, compute the mean rewards 
+            # of the last 100 episodes and log it to wnadb
             if all_rewards:
                 # Compute the mean reward of the last 100 episodes
-                mean_reward=np.mean(all_rewards[-self.check_freq:])
+                mean_reward = np.mean(all_rewards[-self.check_freq:])
                 # Log the mean reward of the last 100 episods to wandb
-                wandb.log({'mean_reward': mean_reward, 'steps': self.num_timesteps})
+                wandb.log({'mean_reward': mean_reward, 
+                           'steps': self.num_timesteps})
         
                 # Save the best model
                 if mean_reward > self.best_mean_reward:
                     self.best_mean_reward=mean_reward
-                    self.model.save(os.path.join(self.save_path, 'best_model'))
+                    self.model.save(os.path.join(self.save_path, 
+                                                 'best_model'))
         
-        # Return True if the callback should be called again, and False otherwise
+        # Return True if the callback should be called again, 
+        # and False otherwise
         return True
 
 
 def make_env(env_id, seed=0):
     '''
-    Function for creating the environment with the correct wrappers and rendering.
+    Function for creating the environment with the correct 
+        wrappers and rendering.
     '''
     def _init():
         # Create the environment with render mode set to human
-        env=gym.make(env_id, render_mode='human')
-        # Set the seed of the environment, this is done to make the results reproducible
+        env = gym.make(env_id, render_mode='human')
+        # Set the seed of the environment, 
+        # this is done to make the results reproducible
         env.seed(seed)
-        # Wrap the environment with the AtariWrapper which wrpas the environment correctly
-        env=AtariWrapper(env)
-        # Wrap the environment with the RewardLogger wrapper which logs the rewards of each episode to wandb
-        env=RewardLogger(env)
+        # Wrap the environment with the AtariWrapper 
+        # which wrpas the environment correctly
+        env = AtariWrapper(env)
+        # Wrap the environment with the RewardLogger wrapper 
+        # which logs the rewards of each episode to wandb
+        env = RewardLogger(env)
         # Return the environement
         return env
     # Return the _init function which is used to create the environment
