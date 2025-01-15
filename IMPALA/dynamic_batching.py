f7abd9854ed122d2e133eeb46767f04ba9f5ca24
yooodleee <accia25@naver.com>
2025-01-15 16:34:32 +0900

    fix code style

--- IMPALA/dynamic_batching.py
+++ IMPALA/dynamic_batching.py
@@ -19,21 +19,18 @@ class _Batcher(object):
     """
     A train layer around the Batcher TensorFlow operations.
 
-    It shares some of the interface with queues (close(), name) to be able to use
-    it correctly as the input to a QueueRunner.
+    It shares some of the interface with queues (close(), name) 
+        to be able to use it correctly as the input to a QueueRunner.
     """
 
-    def __init__(
-        self,
-        minimum_batch_size,
-        maximum_batch_size,
-        timeout_ms
-    ):
-        self._handle = batcher_ops.batcher(
-            minimum_batch_size,
-            maximum_batch_size,
-            timeout_ms or -1
-        )
+    def __init__(self,
+                minimum_batch_size,
+                maximum_batch_size,
+                timeout_ms):
+        
+        self._handle = batcher_ops.batcher(minimum_batch_size,
+                                            maximum_batch_size,
+                                            timeout_ms or -1)
     
     @property
     def name(self):
@@ -43,10 +40,14 @@ class _Batcher(object):
         return batcher_ops.batcher_get_inputs(self._handle, input_dtypes)
     
     def set_outputs(self, flat_result, computation_id):
-        return batcher_ops.batcher_set_outputs(self._handle, flat_result, computation_id)
+        return batcher_ops.batcher_set_outputs(self._handle, 
+                                               flat_result, 
+                                               computation_id)
     
     def compute(self, flat_args, output_dtypes):
-        return batcher_ops.batcher_compute(self._handle, flat_args, output_dtypes)
+        return batcher_ops.batcher_compute(self._handle, 
+                                           flat_args, 
+                                           output_dtypes)
     
     def close(self, cancle_pending_enqueues=False, name=None):
         del cancle_pending_enqueues
@@ -57,18 +58,18 @@ def batch_fn(f):
     """See 'batch_fn_with_options' for details."""
     return batch_fn_with_options()(f)
 
-def batch_fn_with_options(
-    minimum_batch_size=1,
-    maximum_batch_size=1024,
-    timeout_ms=100
-):
+def batch_fn_with_options(minimum_batch_size=1,
+                            maximum_batch_size=1024,
+                            timeout_ms=100):
     """
     Python decorator that automatically batches computations.
 
-    When the decorated function is called, it creates an operation that adds the
-    inputs to a queue, waits until the computation is done, and returns the tensors.
-    The inputs must be nests (see 'tf.contrib.framework.nest') and the first dimensions
-    of each tensor in the nest must have size 1.
+    When the decorated function is called, it creates an operation 
+        that adds the inputs to a queue, waits until the computation 
+        is done, and returns the tensors.
+
+    The inputs must be nests (see 'tf.contrib.framework.nest') and the first 
+        dimensions of each tensor in the nest must have size 1.
 
     It adds a QueueRunner that asynchronously keeps fetching batches of data,
     computes the results and pushes the results back to the caller.
@@ -80,20 +81,22 @@ def batch_fn_with_options(
         def fn(a, b):
             return a + b
         
-        output0 = fn(tf.constant([1]), tf.constant([2]))  # will be batched with the next call.
+        output0 = fn(tf.constant([1]), tf.constant([2]))  # will be batched 
+                    with the next call.
         
         output1 = fn(tf.constant([3]), tf.constant([4])
     
     Note, gradients are currently not supported.
-    Note, if minimum_batch_size == maximum_batch_size and timeout_ms=None, then
-    the batch size of input arguments will be set statically. Otherwise, it will be None.
+    Note, if minimum_batch_size == maximum_batch_size and timeout_ms=None, 
+        then the batch size of input arguments will be set statically. 
+    Otherwise, it will be None.
 
     Args:
         minimum_batch_size: The minimum batch size before processing starts.
         maximum_batch_size: The maximum batch size.
-        timeout_ms: Milliseconds after a batch of samples is requested before it is
-            processed, even if the batch size is smaller than 'minimum_batch_size'. If 
-            None, there is no timeout.
+        timeout_ms: Milliseconds after a batch of samples is requested 
+            before it is processed, even if the batch size is smaller than 
+            'minimum_batch_size'. If None, there is no timeout.
 
     Returns:
         The decorator.
@@ -108,11 +111,12 @@ def batch_fn_with_options(
         def wrapper(*args):
             """Wrapper."""
 
-            flat_args = [tf.convert_to_tensor(arg) for arg in nest.flatten(args)]
+            flat_args = [tf.convert_to_tensor(arg) \
+                         for arg in nest.flatten(args)]
 
             if batcher[0] is None:
-                # Remove control dependencies which is necessary when created in loops,
-                # etc.
+                # Remove control dependencies which is necessary 
+                # when created in loops, etc.
                 with tf.control_dependencies(None):
                     input_dtypes = [t.dtype for t in flat_args]
                     batcher[0] = _Batcher(
@@ -120,15 +124,18 @@ def batch_fn_with_options(
                     
                     # compute in batches using a queue runner.
 
-                    if minimum_batch_size == maximum_batch_size and timeout_ms is None:
+                    if minimum_batch_size == maximum_batch_size and \
+                        timeout_ms is None:
                         batch_size = minimum_batch_size
                     else:
                         batch_size = None
                     
                     # Dequeue batched input.
-                    inputs, computation_id = batcher[0].get_inputs(input_dtypes)
+                    inputs, computation_id = \
+                        batcher[0].get_inputs(input_dtypes)
                     nest.map_structure(
-                        lambda i, a: i.set_shape([batch_size] + a.shape.as_list()[1:]),
+                        lambda i, a: i.set_shape([batch_size] \
+                                                 + a.shape.as_list()[1:]),
                         inputs, flat_args)
                     
                     # Compute result.
@@ -137,15 +144,16 @@ def batch_fn_with_options(
                     flat_result = nest.flatten(result)
 
                     # Insert results back into batcher.
-                    set_op = batcher[0].set_outputs(flat_result, computation_id)
+                    set_op = batcher[0].set_outputs(flat_result, 
+                                                    computation_id)
 
                     tf.compat.v1.train.add_queue_runner(
                         tf.compat.v1.train.QueueRunner(batcher[0], [set_op]))
                 
                 # Insert inputs into input queue.
                 flat_result = batcher[0].compute(
-                    flat_args,
-                    [t.dtype for t in nest.flatten(batched_output[0])])
+                                flat_args,
+                                [t.dtype for t in nest.flatten(batched_output[0])]) 
                 
                 # Restore structure and shapes.
                 result = nest.pack_sequence_as(batched_output[0], flat_result)
