7872782bd418cd1da0dfd6338ab2e758d3673ef3
yooodleee <accia25@naver.com>
2025-01-16 18:24:40 +0900

    fix code style

--- SAC/sac/replay_memory.py
+++ SAC/sac/replay_memory.py
@@ -13,12 +13,12 @@ class ReplayMemory:
     def push(self, state, action, reward, next_state, done):
         if len(self.buffer) < self.capacity:
             self.buffer.append(None)
-        self.buffer[self.position]=[state, action, reward, next_state, done]
-        self.position=(self.position + 1) % self.capacity
+        self.buffer[self.position] = [state, action, reward, next_state, done]
+        self.position = (self.position + 1) % self.capacity
     
     def sample(self, batch_size):
-        batch=random.sample(self.buffer, batch_size)
-        state, action, reward, next_state, done=map(np.stack(), zip(*batch))
+        batch = random.sample(self.buffer, batch_size)
+        state, action, reward, next_state, done = map(np.stack(), zip(*batch))
         return state, action, reward, next_state, done
     
     def __len__(self):
@@ -29,7 +29,8 @@ class ReplayMemory:
             os.makedirs("checkpoints/")
         
         if save_path is None:
-            save_path="checkpoints/sac_buffer_{}_{}".format(env_name, suffix)
+            save_path = "checkpoints/sac_buffer_{}_{}".format(env_name, 
+                                                              suffix)
         print("Saving buffer to {}".format(save_path))
 
         with open(save_path, "wb") as f:
@@ -39,5 +40,5 @@ class ReplayMemory:
         print("Loading buffer from {}".format(save_path))
 
         with open(save_path, "rb") as f:
-            self.buffer=pickle.load(f)
-            self.position=len(self.buffer) % self.capacity
+            self.buffer = pickle.load(f)
+            self.position = len(self.buffer) % self.capacity
