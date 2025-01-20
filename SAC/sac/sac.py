f8948ed7087ff2460b3a78f3b05f7d733669cb53
yooodleee <accia25@naver.com>
2025-01-20 19:31:32 +0900

    update sac.py

--- SAC/sac/sac.py
+++ SAC/sac/sac.py
@@ -17,45 +17,42 @@ class SAC(object):
         self.target_update_interval = args.target_update_interval
         self.automatic_entropy_tuning = args.automatic_entropy_tuning
 
-        self.device = torch.device("cuda" \
-                                   if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        self.critic = QNetwork(num_inputs, 
-                               action_space.shape[0], 
-                               args.hidden_size).to(device=self.device)
-        self.critic_optim = Adam(self.critic.parameters(), 
-                                 lr=args.lr)
+        self.critic = QNetwork(num_inputs,  # num_inputs
+                               action_space.shape[0],   # num_actions
+                               args.hidden_size).to(device=self.device) # hidden_dim
+        self.critic_optim = Adam(self.critic.parameters(), lr=args.lr)
         
-        self.critic_target = QNetwork(num_inputs, 
-                                      action_space.shape[0], 
-                                      args.hidden_size).to(self.device)
-        hard_update(self.critic_target, self.critic)
+        self.critic_target = QNetwork(num_inputs,   # num_inputs
+                                      action_space.shape[0],    # num_actions
+                                      args.hidden_size).to(self.device) # hidden_dim        
+        hard_update(self.critic_target, self.critic)    # target(critic_target), source(critic)
 
         if self.policy_type == "Gaussian":
             # Target Entropy = -dim(A) (e.g. / -6 for HalfCheetah-v2) 
             # as given in the paper
             if self.automatic_entropy_tuning is True:
-                self.target_entropy = \
-                    -torch.prod(torch.Tensor(action_space.shape).\
-                                to(self.device)).item()
-                self.log_alpha = torch.zeros(1, 
-                                             requires_grad=True, 
-                                             device=self.device)
+                # returns the product of all elements in the `input` tensor.
+                self.target_entropy = -torch.prod(torch.Tensor(action_space.shape).to(self.device)).item()
+                # returns a tensor filled with the scalar value `0`
+                # with the shape defined by the variable argumen `size(=1)`
+                self.log_alpha = torch.zeros(1, requires_grad=True, device=self.device)
                 self.alpha_optim = Adam([self.log_alpha], lr=args.lr)
             
-            self.policy = GaussianPolicy(num_inputs, 
-                                         action_space.shape[0], 
-                                         args.hidden_size, 
-                                         action_space).to(self.device)
+            self.policy = GaussianPolicy(num_inputs,    # num_inputs
+                                         action_space.shape[0],     # num_actions
+                                         args.hidden_size,  # hidden_dim
+                                         action_space).to(self.device)  # action_space
             self.policy_optim = Adam(self.policy.parameters(), lr=args.lr)
 
         else:
             self.alpha = 0
             self.automatic_entropy_tuning = False
-            self.policy = DeterministicPolicy(num_inputs, 
-                                              action_space.shape[0], 
-                                              args.hidden_size, 
-                                              action_space).to(self.device)
+            self.policy = DeterministicPolicy(num_inputs, # num_inputs
+                                              action_space.shape[0],  # num_actions
+                                              args.hidden_size,     # hidden_dim
+                                              action_space).to(self.device) # action_space
             self.policy_optim = Adam(self.policy.parameters(), lr=args.lr)
     
     def select_action(self, state, evaluate=False):
@@ -68,8 +65,7 @@ class SAC(object):
 
     def update_parameters(self, memory, batch_size, updates):
         # Sample a batch from memory
-        state_batch, action_batch, reward_batch, next_state_batch, \
-            mask_batch = memory.sample(batch_size=batch_size)
+        state_batch, action_batch, reward_batch, next_state_batch, mask_batch = memory.sample(batch_size=batch_size)
 
         state_batch = torch.FloatTensor(state_batch).to(self.device)
         next_state_batch = torch.FloatTensor(next_state_batch).to(self.device)
@@ -78,14 +74,10 @@ class SAC(object):
         mask_batch = torch.FloatTensor(mask_batch).to(self.device)
 
         with torch.no_grad():
-            next_state_action, \
-                next_state_log_pi, _ = self.policy.sample(next_state_batch)
-            qf1_next_target, \
-                qf2_next_target = self.critic_target(next_state_batch, 
-                                                     next_state_action)
-            min_qf_next_target = torch.min(qf1_next_target, 
-                                           qf2_next_target) \
-                                            - self.alpha * next_state_log_pi
+            next_state_action, next_state_log_pi, _ = self.policy.sample(next_state_batch)
+            qf1_next_target, qf2_next_target = self.critic_target(next_state_batch, next_state_action)
+            # returns the minimum value of all elements in the `input` tensor.
+            min_qf_next_target = torch.min(qf1_next_target, qf2_next_target) - self.alpha * next_state_log_pi
             next_q_value = reward_batch \
                             + mask_batch \
                             * self.gamma \
@@ -99,8 +91,10 @@ class SAC(object):
         qf2_loss = F.mse_loss(qf2, next_q_value)  
         qf_loss = qf1_loss + qf2_loss
 
+        # reset the gradients of all optimized `torch.Tensor`
         self.critic_optim.zero_grad()
         qf_loss.backward()
+        # perform a single optimization step
         self.critic_optim.step()
 
         pi, log_pi, _ = self.policy.sample(state_batch)
@@ -116,8 +110,7 @@ class SAC(object):
         self.policy_optim.step()
 
         if self.automatic_entropy_tuning:
-            alpha_loss = -(self.log_alpha \
-                           * (log_pi + self.target_entropy).detach()).mean()
+            alpha_loss = -(self.log_alpha * (log_pi + self.target_entropy).detach()).mean()
 
             self.alpha_optim.zero_grad()
             alpha_loss.backward()
@@ -132,28 +125,24 @@ class SAC(object):
         if updates % self.target_update_interval == 0:
             soft_update(self.critic_target, self.critic, self.tau)
         
-        return qf1_loss.item(), \
-                qf2_loss.item(), \
-                policy_loss.item(), \
-                alpha_loss.item(), \
-                alpha_tlogs.item()
+        return qf1_loss.item(), qf2_loss.item(), policy_loss.item(), alpha_loss.item(), alpha_tlogs.item()
     
     # Save model parameters
     def save_checkpoint(self, env_name, suffix="", ckpt_path=None):
         if not os.path.exists("checkpoints/"):
             os.makedirs("checkpoints/")
         if ckpt_path is None:
-            ckpt_path="checkpoint/sac_checkpoint_{}_{}".format(env_name, 
-                                                                suffix)
+            ckpt_path="checkpoint/sac_checkpoint_{}_{}".format(env_name, suffix)
         print("Saving models to {}".format(ckpt_path))
-        torch.save({"policy_state_dict":self.policy.state_dict(),
-                    "critic_state_dict":self.critic.state_dict(),
-                    "critic_target_state_dict":\
-                        self.critic_target.state_dict(),
-                    "critic_optimizer_state_dict":\
-                        self.critic_optim.state_dict(),
-                    "policy_optimizer_state_dict":\
-                        self.policy_optim.state()}, ckpt_path)
+        torch.save({
+            "policy_state_dict": self.policy.state_dict(),
+            "critic_state_dict": self.critic.state_dict(),
+            "critic_target_state_dict": self.critic_target.state_dict(),
+            "critic_optimizer_state_dict": self.critic_optim.state_dict(),
+            "policy_optimizer_state_dict": self.policy_optim.state(),
+            }, 
+            ckpt_path
+        )
     
     # Load model parameters
     def load_checkpoint(self, ckpt_path, evaluate=False):
@@ -162,12 +151,9 @@ class SAC(object):
             checkpoint = torch.load(ckpt_path)
             self.policy.load_state_dict(checkpoint["policy_state_dict"])
             self.policy.load_state_dict(checkpoint["critic_state_dict"])
-            self.critic_target.load_state_dict(
-                checkpoint["critic_target_state_dict"])
-            self.critic_optim.load_state_dict(
-                checkpoint["critic_optimizer_state_dict"])
-            self.policy_optim.load_state_dict(
-                checkpoint["policy_optimizer_state_dict"])
+            self.critic_target.load_state_dict(checkpoint["critic_target_state_dict"])
+            self.critic_optim.load_state_dict(checkpoint["critic_optimizer_state_dict"])
+            self.policy_optim.load_state_dict(checkpoint["policy_optimizer_state_dict"])
 
             if evaluate:
                 self.policy.eval()
