d4861d9c90ee5ebcf659586dbf97415d10ad27dc
yooodleee <accia25@naver.com>
2025-01-20 20:29:35 +0900

    create GoString class

--- Alphago/dlgo/goboard.py
+++ Alphago/dlgo/goboard.py
@@ -104,3 +104,43 @@ class Move():
         return Move(is_pass=True)
 
 
+# chain of connected stones (used to e.g. efficiently check for liberties)
+class GoString():
+    def __init__(
+            self,
+            color,
+            stones,
+            liberties):
+        
+        self.color = color
+        self.stones = frozenset(stones)
+        self.liberties = frozenset(liberties)
+    
+    def without_liberty(self, point):
+        new_liberties = self.liberties - set([point])
+        return GoString(self.color, self.stones, new_liberties)
+    
+    def with_liberty(self, point):
+        new_liberties = self.liberties | set([point])
+        return GoString(self.color, self.stones, new_liberties)
+    
+    def merged_with(self, go_string):
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones
+        return GoString(
+            self.color,
+            combined_stones,
+            (self.liberties | go_string.liberties) - combined_stones
+        )
+    
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
+    
+    def __eq__(self, other):
+        return isinstance(other, GoString) and \
+            self.color == other.color and \
+            self.stones == other.stones and \
+            self.liberties == other.liberties
+
+
