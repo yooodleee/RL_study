674fb1ed1aa56da9130384c340f9094967e5e0be
yooodleee <accia25@naver.com>
2025-01-20 21:12:18 +0900

    create GoString class

--- Alphago/dlgo/goboard_fast.py
+++ Alphago/dlgo/goboard_fast.py
@@ -49,3 +49,53 @@ def init_corner_table(dim):
     corner_tables[dim] = new_table
 
 
+class IllegalMoveError(Exception):
+    pass
+
+
+class GoString():
+    def __init__(
+            self,
+            color,
+            stones,
+            liberties):
+        
+        self.color = color
+        self.stones = stones
+        self.liberties = liberties
+    
+    def without_liberty(self, point):
+        new_liberties = self.liberties - set([point])
+        return GoString(self.color, self.stones, new_liberties)
+    
+    def with_liberty(self, point):
+        new_liberties = self.liberties | set([point])
+        return GoString(self.colir, self.stones, new_liberties)
+    
+    def merged_with(self, string):
+        assert string.color == self.color 
+        combined_stones = self.stones | string.stones
+        return GoString(
+            self.color,
+            combined_stones,
+            (self.liberties | string.liberties) - combined_stones
+        )
+    
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
+    
+    def __eq__(self, other):
+        return isinstance(other, GoString) and \
+            self.color == other.color and \
+            self.stones == other.stones and \
+            self.liberties == other.liberties
+    
+    def __deepcopy__(self, memodict={}):
+        return GoString(
+            self.color,
+            self.stones,
+            copy.deepcopy(self.liberties)
+        )
+
+
