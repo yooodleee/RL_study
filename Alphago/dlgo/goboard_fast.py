c3578d4101630f7e34e54bd8e90978ef380cc7cd
yooodleee <accia25@naver.com>
2025-01-20 21:07:41 +0900

    create init_corner_table method

--- Alphago/dlgo/goboard_fast.py
+++ Alphago/dlgo/goboard_fast.py
@@ -25,7 +25,27 @@ def init_neighbor_table(dim):
                 n for n in full_neighbors
                 if 1 <= n.row <= rows and 1 <= n.col <= cols
             ]
-        new_table[p] = true_neighbors
+            new_table[p] = true_neighbors
     neighbor_tables[dim] = new_table
+
+
+def init_corner_table(dim):
+    rows, cols = dim
+    new_table = {}
+    for r in range(1, rows + 1):
+        for c in range(1, cols + 1):
+            p = Point(row=r, col=c)
+            full_corners = [
+                Point(row=p.row - 1, col=p.col - 1),
+                Point(row=p.row - 1, col=p.col + 1),
+                Point(row=p.row + 1, col=p.col - 1),
+                Point(row=p.row + 1, col=p.col + 1),
+            ]
+            true_corners = [
+                n for n in full_corners
+                if 1 <= n.row <= rows and 1 <= n.col <= cols
+            ]
+            new_table[p] = true_corners
+    corner_tables[dim] = new_table
 
 
