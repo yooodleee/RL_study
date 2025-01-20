7a5b6dbb6c3fb66e793ce942737acb7845df8db8
yooodleee <accia25@naver.com>
2025-01-20 18:19:18 +0900

    fix code style and add some docstring

--- SAC/sac/main.py
+++ SAC/sac/main.py
@@ -1,15 +1,15 @@
-import argparse
+import argparse # easily manage model's hyperparameters
 import datetime
 import gym
 import numpy as np
-import itertools
+import itertools    # automatically generate iterator
 import torch
-import mujoco_py
-from sac import SAC
+import mujoco_py    # advanced physics simulator
+from .sac import SAC
 from torch.utils.tensorboard import summary
 from replay_memory import ReplayMemory
 
-parser=argparse.ArgumentParser(description="PyTorch Soft Actor-Critic Args")
+parser = argparse.ArgumentParser(description="PyTorch Soft Actor-Critic Args")
 parser.add_argument(
     "--env-name", 
     default="HalfCheetah-v4", 
@@ -123,12 +123,10 @@ agent=SAC(env.observation_space.shape[0],
           args)
 
 # Tensorboard
-writer = summary("runs/{}_SAC_{}_{}_{}".\
-                 format(datetime.datetime.\
-                        now().strftime("%Y-%m-%d-%H-%M-%S"), 
-                        args.env_name, 
-                        args.policy, 
-                        "autotime" if args.automatic_entropy_tuning else ""))
+writer = summary("runs/{}_SAC_{}_{}_{}".format(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"), 
+                 args.env_name, 
+                 args.policy, 
+                 "autotime" if args.automatic_entropy_tuning else ""))
 
 # Memory
 memory = ReplayMemory(args.replay_size, args.seed)
@@ -153,11 +151,9 @@ for i_episode in itertools.count(1):
             # Number of updates per step in environment
             for i in range(args.updates_per_step):
                 # Update parameters of all the networks
-                critic_1_loss, critic_2_loss, \
-                    policy_loss, ent_loss, alpha = agent.\
-                        update_parameters(memory, 
-                                          args.batch_size, 
-                                          updates)
+                critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(memory, 
+                                                                                                     args.batch_size, 
+                                                                                                     updates)
 
                 writer.add_scalar("loss/critic_1", critic_1_loss, updates)
                 writer.add_scalar("loss/critic_2", critic_2_loss, updates)
@@ -172,47 +168,43 @@ for i_episode in itertools.count(1):
         episode_reward += reward
 
         # Ignore the "done" signal if it comes from hitting the time horizon.
-        mask=1 if episode_steps == env._max_episode_steps else float(not done)
+        mask = 1 if episode_steps == env._max_episode_steps else float(not done)
 
         # Append transition to memory
         memory.push(state, action, reward, next_state, mask)    
 
-        state=next_state
+        state = next_state
     
     if total_numsteps > args.num_steps:
         break
 
     writer.add_scalar("reward/train", episode_reward, i_episode)
-    print("Episode: {}, \
-          total numsteps: {}, \
-          episode steps: {}, \
-          reward: {}".format(i_episode, 
-                             total_numsteps, 
-                             episode_steps, 
-                             round(episode_reward, 2)))
+    print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}".format(i_episode, 
+                                                                                  total_numsteps, 
+                                                                                  episode_steps, 
+                                                                                  round(episode_reward, 2)))
 
     if i_episode % 10 == 0 and args.eval is True:
-        avg_reward=0.
-        episodes=10
+        avg_reward = 0.
+        episodes = 10
         for _ in range(episodes):
-            state=env.reset()
-            episode_reward=0
-            done=False
+            state = env.reset()
+            episode_reward = 0
+            done = False
             while not done:
-                action = agent.select_action(state, evaluate=True)
+                action = agent.select_action(state, evaluate=True)  # sample action from policy
 
                 next_state, reward, done, _ = env.step(action)
                 episode_reward += reward
 
-                state=next_state
+                state = next_state
             avg_reward += episode_reward
         avg_reward /= episodes
 
         writer.add_scalar("avg_reward/test", avg_reward, i_episode)
 
         print("----------------------------------------")
-        print("Test Episodes: {}, \
-              Avg. Reward: {}".format(episodes, round(avg_reward, 2)))
+        print("Test Episodes: {}, Avg. Reward: {}".format(episodes, round(avg_reward, 2)))
         print("----------------------------------------")
 
 env.close()
