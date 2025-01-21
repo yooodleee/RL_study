efe10f25a0349dffe11f82da7617de583a8606ab
yooodleee <accia25@naver.com>
2025-01-21 11:25:55 +0900

    create GameState class

--- Alphago/dlgo/goboard_fast.py
+++ Alphago/dlgo/goboard_fast.py
@@ -306,3 +306,119 @@ class Move():
         )
 
 
+class GameState():
+    def __init__(
+            self,
+            board,
+            next_player,
+            previous,
+            move):
+        
+        self.board = board
+        self.next_player = next_player
+        self.previous_state = previous
+        if previous is None:
+            self.previous_state = frozenset()
+        else:
+            self.previous_state = frozenset(
+                previous.previous_state |
+                {
+                    (previous.next_player, previous.board.zobrist_hash())
+                }
+            )
+        self.last_move = move
+    
+    def apply_move(self, move):
+        if move.is_play:
+            next_board = copy.deepcopy(self.board)
+            next_board.place_stone(self.next_player, move.point)
+        else:
+            next_board = self.board
+        return GameState(
+            next_board,
+            self.next_player.other,
+            self,
+            move,
+        )
+    
+    @classmethod
+    def new_game(cls, board_size):
+        if isinstance(board_size, int):
+            board_size = (board_size, board_size)
+        board = Board(*board_size)
+        return GameState(
+            board,
+            Player.black,
+            None,
+            None,
+        )
+    
+    def is_move_self_capture(
+            self,
+            player,
+            move):
+        
+        if not move.is_play:
+            return False
+        return self.board.is_self_capture(player, move.point)
+    
+    @property
+    def situation(self):
+        return (self.next_player, self.board)
+    
+    def does_move_violate_ko(
+            self,
+            player,
+            move):
+        
+        if not move.is_play:
+            return False
+        if not self.board.will_capture(player, move.point):
+            return False
+        next_board = copy.deepcopy(self.board)
+        next_board.place_stone(player, move.point)
+        next_situation = (player.other, next_board.zobrist_hahs())
+        return next_situation in self.previous_state
+    
+    def is_valid_move(self, move):
+        if self.is_over():
+            return False
+        if move.is_pass or move.is_resign:
+            return True
+        return (
+            self.board.get(move.point) is None and
+            not self.is_move_self_capture(self.next_player, move) and
+            not self.does_move_violate_ko(self.next_player, move)
+        )
+    
+    def is_over(self):
+        if self.last_move is None:
+            return False
+        if self.last_move.is_resign:
+            return True
+        second_last_move = self.previous_state.last_move
+        if second_last_move is None:
+            return False
+        return self.last_move.is_pass and second_last_move.is_pass
+
+    def legal_moves(self):
+        if self.is_over():
+            return []
+        moves = []
+        for row in range(1, self.board.num_rows + 1):
+            for col in range(1, self.board.num_cols + 1):
+                move = Move.play(Point(row, col))
+                if self.is_valid_move(move):
+                    moves.append(move)
+        # these two moves are always legal
+        moves.append(Move.pass_turn())
+        moves.append(Move.resign())
+        return moves
+    
+    def winner(self):
+        if not self.is_over():
+            return None
+        if self.last_move.is_resign:
+            return self.next_player
+        game_result = compute_game_result(self)
+        return game_result.winner
