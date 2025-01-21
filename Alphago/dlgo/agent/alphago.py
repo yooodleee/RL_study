0d92ee9cae2d6b672501c7ff91937496ccff2f29
yooodleee <accia25@naver.com>
2025-01-21 14:41:12 +0900

    add AlphaGoMCTS

--- Alphago/dlgo/agent/alphago.py
+++ Alphago/dlgo/agent/alphago.py
@@ -47,3 +47,92 @@ class AlphaGoNode:
                 * self.prior_value / (1 + self.visit_count)
 
 
+class AlphaGoMCTS(Agent):
+    def __init__(
+            self,
+            policy_agent,
+            fast_policy_agent,
+            value_agent,
+            lambda_value=0.5,
+            num_simulations=1000,
+            depth=50,
+            rollout_limit=100):
+        
+        self.policy = policy_agent
+        self.rollout_policy = fast_policy_agent
+        self.value = value_agent
+        self.lambda_value = lambda_value
+        self.num_simulations = num_simulations
+        self.depth = depth
+        self.rollout_limit = rollout_limit
+        self.root = AlphaGoNode()
+    
+    def select_move(self, game_state):
+        for simulation in range(self.num_simulations):
+            current_state = game_state
+            node = self.root
+            for depth in range(self.depth):
+                if not node.children:
+                    if current_state.is_over():
+                        break
+                    moves, probabilities = self.policy_probabilities(current_state)
+                    nodes.expand_children(moves, probabilities)
+                move, node = current_state.apply_move(move)
+            value = self.value.predict(current_state)
+            rollout = self.policy_rollout(current_state)
+            weighted_value = (1 - self.lambda_value) * value + \
+                self.lambda_value * rollout
+            node.update_values(weighted_value)
+        
+        move = max(
+            self.root.children,
+            key=lambda move: self.root.children.get(move).visit_count
+        )
+
+        self.root = AlphaGoNode()
+        if move in self.root.children:
+            self.root = self.root.children[move]
+            self.root.parent = None
+        
+        return move
+    
+    def policy_probabilities(self, game_state):
+        encoder = self.policy._encoder
+        outputs = self.policy.predict(game_state)
+        legal_moves = game_state.legal_moves()
+        if not legal_moves:
+            return [], []
+        encoded_points = [
+            encoder.encode_point(move.point)
+            for move in legal_moves
+            if move.point
+        ]
+        legal_outputs = outputs[encoded_points]
+        normalized_outputs = legal_outputs / np.sum(legal_outputs)
+        return legal_moves, normalized_outputs
+    
+    def policy_rollout(self, game_state):
+        for step in range(self.rollout_limit):
+            if game_state.is_over():
+                break
+            move_probabilities = self.rollout_policy.predict(game_state)
+            encoder = self.rollout_policy._encoder
+            valid_moves = [
+                m for idx, m in enumerate(valid_moves)
+                if Move(encoder.decode_point_index(idx) in game_state.legal_moves())
+            ]
+            max_index, max_value = max(
+                enumerate(valid_moves),
+                key=operator.itemgetter(1),
+            )
+            max_point = encoder.decode_point_index(max_index)
+            greedy_move = Move(max_point)
+            if greedy_move in game_state.legal_moves():
+                game_state = game_state.apply_move(greedy_move)
+        
+        next_player = game_state.next_player
+        winner = game_state.winner()
+        if winner is not None:
+            return 1 if winner == next_player else -1
+        else:
+            return 0
