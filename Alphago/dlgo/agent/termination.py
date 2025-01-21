c98b4cf1e18d71a4e5682d26a6161119144f9593
yooodleee <accia25@naver.com>
2025-01-21 17:52:36 +0900

    add ResignLargeMargin

--- Alphago/dlgo/agent/termination.py
+++ Alphago/dlgo/agent/termination.py
@@ -20,3 +20,30 @@ class PassWhenOpponentPasses(TerminationStrategy):
             return True if game_state.last_move.is_pass else False
 
 
+class ResignLargeMargin(TerminationStrategy):
+    def __init__(
+            self,
+            own_color,
+            cut_off_move,
+            margin):
+        
+        TerminationStrategy.__init__(self)
+        self.own_color = own_color
+        self.cut_off_move = cut_off_move
+        self.margin = margin
+
+        self.moves_played = 0
+    
+    def should_pass(self, game_state):
+        return False
+    
+    def should_resign(self, game_state):
+        self.moves_played += 1
+        if self.moves_played:
+            game_result = scoring.compute_game_result(self)
+            if game_result.winner != self.own_color and game_result.winning_margin >= self.margin:
+                return True
+        
+        return False
+
+
