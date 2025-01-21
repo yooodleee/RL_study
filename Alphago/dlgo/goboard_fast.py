e3391e3fc46925aebb3e64c9721d79208ea8a9f2
yooodleee <accia25@naver.com>
2025-01-21 11:15:29 +0900

    create Move class

--- Alphago/dlgo/goboard_fast.py
+++ Alphago/dlgo/goboard_fast.py
@@ -250,3 +250,59 @@ class Board():
         return self._hash
 
 
+class Move():
+    def __init__(
+            self,
+            point=None,
+            is_pass=False,
+            is_resign=False):
+        
+        assert(point is not None) ^ is_pass ^ is_resign
+        self.point = point
+        self.is_play = (self.point is not None)
+        self.is_pass = is_pass
+        self.is_resign = is_resign
+    
+    @classmethod
+    def play(cls, point):
+        return Move(point=point)
+    
+    @classmethod
+    def pass_turn(cls):
+        return Move(is_pass=True)
+    
+    @classmethod
+    def resign(cls):
+        return Move(is_resign=True)
+    
+    def __str__(self):
+        if self.is_pass:
+            return 'pass'
+        if self.is_resign:
+            return 'resign'
+        return '(r %d, c %d)' % (self.point.row, self.point.col)
+    
+    def __hash__(self):
+        return hash(
+            (
+                self.is_play,
+                self.is_pass,
+                self.is_resign,
+                self.point,
+            )
+        )
+    
+    def __eq__(self, other):
+        return (
+            self.is_play,
+            self.is_pass,
+            self.is_resign,
+            self.point,
+        ) == (
+            other.is_play,
+            other.is_pass,
+            other.is_resign,
+            other.point,
+        )
+
+
