6dc96c3ecdab793a6e93f9fd584a89272ce804b9
yooodleee <accia25@naver.com>
2025-01-21 12:09:28 +0900

    create Point

--- Alphago/dlgo/gotypes.py
+++ Alphago/dlgo/gotypes.py
@@ -11,3 +11,11 @@ class Player(enum.Enum):
         return Player.black if self == Player.white else Player.white
 
 
+class Point(namedtuple('Point', 'row col')):
+    def neighbors(self):
+        return [
+            Point(self.row - 1, self.col),
+            Point(self.row + 1, self.col),
+            Point(self.row, self.col - 1),
+            Point(self.row, self.col + 1),
+        ]
