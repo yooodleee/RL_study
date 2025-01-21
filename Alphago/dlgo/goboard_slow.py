9b975cdcc5af73978e18b73b1e2f1eaa348a9810
yooodleee <accia25@naver.com>
2025-01-21 11:41:28 +0900

    chain of connected stones

--- Alphago/dlgo/goboard_slow.py
+++ Alphago/dlgo/goboard_slow.py
@@ -96,3 +96,42 @@ class Move():
         return Move(is_resign=True)
 
 
+# chain of connected stones
+# (used to e.g. efficiently check for liberties)
+class GoString():
+    def __init__(
+            self,
+            color,
+            stones,
+            liberties):
+        
+        self.color = color
+        self.stones = set(stones)
+        self.liberties = set(liberties)
+    
+    def remove_liberty(self, point):
+        self.liberties.remove(point)
+    
+    def add_liberty(self, point):
+        self.liberties.add(point)
+    
+    def merged_with(self, go_string):
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones
+        return GoString(
+            self.color,
+            combined_stones,
+            (self.liberties | go_string.liberties) - combined_stones
+        )
+    
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
+    
+    def __eq__(self, other):
+        return isinstance(other, GoString) and \
+            self.color == other.color and \
+            self.stones == other.stones and \
+            self.liberties == other.liberties
+
+
