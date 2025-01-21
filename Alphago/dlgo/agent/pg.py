e0a8aaca146d52348a0ff9b604ed62dfbffc3286
yooodleee <accia25@naver.com>
2025-01-21 15:20:44 +0900

    add PolicyAgent

--- Alphago/dlgo/agent/pg.py
+++ Alphago/dlgo/agent/pg.py
@@ -28,3 +28,83 @@ def prepare_experience_data(
     return target_vectors
 
 
+class PolicyAgent(Agent):
+    def __init__(self, model, encoder):
+        self._model = model
+        self._encoder = encoder
+        self._collector = None
+    
+    def set_collector(self, collector):
+        self._collector = collector
+    
+    def select_move(self, game_state):
+        board_tensor = self._encoder.encode(game_state)
+        X = np.array([board_tensor])
+        move_probs = self._model.predict(X)[0]
+        eps = 1e-5
+        move_probs = np.clip(move_probs, eps, 1 -eps)
+        move_probs = move_probs / np.sum(move_probs)
+        num_moves = self._encoder.board_width * self._encoder.board_height
+        candidates = np.arange(num_moves)
+        ranked_moves = np.random.choice(
+            candidates,
+            num_moves,
+            replace=False,
+            p=move_probs,
+        )
+        for point_idx in ranked_moves:
+            point = self._encoder.decode_point_index(point_idx)
+            move = goboard.Move.play(point)
+            is_valid = game_state.is_valid_move(move)
+            is_an_eye = is_point_an_eye(
+                game_state.board,
+                point,
+                game_state.next_player,
+            )
+            if is_valid and (not is_an_eye):
+                if self._collector is not None:
+                    self._collector.record_decision(
+                        state=board_tensor,
+                        action=point_idx,
+                    )
+                return goboard.Move.play(point)
+        return goboard.Move.pass_turn()
+    
+    def serialize(self, h5file):
+        h5file.create_group('encoder')
+        h5file['encoder'].attrs['name'] = self._encoder.name()
+        h5file['encoder'].attrs['board_width'] = self._encoder.board_width
+        h5file['encoder'].attrs['board_height'] = self._encoder.board_height
+        h5file.create_group('model')
+        kerasutil.save_model_to_hdf5_group(
+            self._model,
+            h5file['model'],
+        )
+    
+    def train(
+            self,
+            experience,
+            lr,
+            clipnorm,
+            batch_size):
+        
+        self._model.compile(
+            loss='categorical_crossentropy',
+            optimizer=optimizers.SGD(lr=lr, clipnorm=clipnorm),
+        )
+        
+        # includes tensors with board state where the chosen move is the 
+        # reward (1 / -1) and the rest is 0
+        target_vectors = prepare_experience_data(
+            experience,
+            self._encoder.board_width,
+            self._encoder.board_height,
+        )
+
+        self._model.fit(
+            experience.states,
+            target_vectors,
+            batch_size=batch_size,
+            epochs=1,
+        )
+
