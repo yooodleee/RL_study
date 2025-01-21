ae6a16eebe2309f9e488ba1c4f4d35ff2961982e
yooodleee <accia25@naver.com>
2025-01-21 12:08:10 +0900

    create Player

---
+++ Alphago/dlgo/gotypes.py
@@ -0,0 +1,13 @@
+import enum
+from collections import namedtuple
+
+
+class Player(enum.Enum):
+    black = 1
+    white = 2
+
+    @property
+    def other(self):
+        return Player.black if self == Player.white else Player.white
+
+
