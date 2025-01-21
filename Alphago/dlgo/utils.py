f8ea676b621d4be80703b01e11aa28251bcc4e41
yooodleee <accia25@naver.com>
2025-01-21 14:12:10 +0900

    return coords from point

--- Alphago/dlgo/utils.py
+++ Alphago/dlgo/utils.py
@@ -36,3 +36,10 @@ def point_from_coords(coords):
     return gotypes.Point(row=row, col=col)
 
 
+def coords_from_point(point):
+    return '%s%d' % (
+        COLS[point.col - 1],
+        point.row,
+    )
+
+
