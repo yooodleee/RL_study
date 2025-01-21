b0633034fb0ed725832b660662a1c8de2525c98d
yooodleee <accia25@naver.com>
2025-01-21 14:09:45 +0900

    print board state

--- Alphago/dlgo/utils.py
+++ Alphago/dlgo/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 from dlgo import gotypes
 
-COLS = 'ABCDEFGHRJKLMNOPQRST'
+COLS = 'ABCDEFGHJKLMNOPQRST'
 STONE_TO_CHAR = {
     None: ' . ',
     gotypes.Player.black: ' x ',
@@ -17,5 +17,16 @@ def print_move(player, move):
     else:
         move_str = '%s%d' % (COLS[move.point.col - 1], move.point.row)
     print('%s %s' % (player, move_str))
+
+
+def print_board(board):
+    for row in range(board.num_rows, 0, -1):
+        bump = ' ' if row <= 9 else ''
+        line = []
+        for col in range(1, board.num_cols + 1):
+            stone = board.get(gotypes.Point(row=row, col=col))
+            line.append(STONE_TO_CHAR[stone])
+        print('%s%d %s' % (bump, row, ''.join(line)))
+    print('     ' + '   '.join(COLS[:board.num_cols]))
 
 
