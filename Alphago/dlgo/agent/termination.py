5d2fad932f9d09493c8b0a1d456c2697f7c52e0e
yooodleee <accia25@naver.com>
2025-01-21 18:27:57 +0900

    add TerminationAgent

--- Alphago/dlgo/agent/termination.py
+++ Alphago/dlgo/agent/termination.py
@@ -47,3 +47,30 @@ class ResignLargeMargin(TerminationStrategy):
         return False
 
 
+class TerminationAgent(Agent):
+    def __init__(
+            self,
+            agent,
+            strategy=None):
+        
+        Agent.__init__(self)
+        self.agent = agent
+        self.strategy = strategy if strategy is not None \
+            else TerminationStrategy()
+    
+    def select_move(self, game_state):
+        if self.strategy.should_pass(game_state):
+            return goboard.Move.pass_turn()
+        elif self.strategy.should_resign(game_state):
+            return goboard.Move.resign()
+        else:
+            return self.agent.select_move(game_state)
+
+
+def get(termination):
+    if termination == 'opponent_passes':
+        return PassWhenOpponentPasses()
+    else:
+        raise ValueError(
+            'Unsupported termination strategy: {}'.format(termination)
+        )
