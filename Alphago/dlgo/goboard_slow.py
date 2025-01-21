7b6de9eead07ff78c818485ec8247bb0dc380daa
yooodleee <accia25@naver.com>
2025-01-21 11:49:04 +0900

    create GameState class

--- Alphago/dlgo/goboard_slow.py
+++ Alphago/dlgo/goboard_slow.py
@@ -135,3 +135,82 @@ class GoString():
             self.liberties == other.liberties
 
 
+class GameState():
+    def __init__(
+            self,
+            board,
+            next_player,
+            previous,
+            move):
+        
+        self.board = board
+        self.next_player = next_player
+        self.previous_state = previous
+        self.last_move = move
+    
+    def apply_move(self, move):
+        if move.is_play:
+            next_board = copy.deepcopy(self.board)
+            next_board.place_stone(self.next_player, move.point)
+        else:
+            next_board = self.board
+        return GameState(
+            next_board,
+            self.next_player.other,
+            self,
+            move,
+        )
+    
+    def is_over(self):
+        if self.last_move is None:
+            return False
+        if self.last_move.is_resign:
+            return True
+        second_last_move = self.previous_state.last_move
+        if second_last_move is None:
+            return False
+        return self.last_move.is_pass and second_last_move.is_pass
+    
+    def is_move_self_capture(
+            self,
+            player,
+            move):
+        
+        if not move.is_play:
+            return False
+        next_board = copy.deepcopy(self.board)
+        next_board.place_stone(player, move.point)
+        new_string = next_board.get_go_string(move.point)
+        return new_string.num_liberties == 0
+    
+    def is_valid_move(self, move):
+        if self.is_over():
+            return False
+        if move.is_pass or move.is_resign:
+            return True
+        return (
+            self.board.get(move.point) is None and
+            not self.is_move_self_capture(self.next_player, move) and
+            not self.does_move_violate_ko(self.next_player, move)
+        )
+    
+    def legal_moves(self):
+        moves = []
+        for row in range(1, self.board.num_rows + 1):
+            for col in range(1, self.board.num_cols + 1):
+                move = Move.play(Point(row, col))
+                if self.is_valid_move(move):
+                    moves.append(move)
+        moves.append(Move.pass_turn())
+        moves.append(Move.resign())
+        return moves
+    
+    def winner(self):
+        if not self.is_over():
+            return None
+        if self.last_move.is_resign:
+            return self.next_player
+        game_result = compute_game_result(self)
+        return game_result.winner
+    
+    
