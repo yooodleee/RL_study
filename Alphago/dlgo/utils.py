788dd02fd9252c8fb5895d76f2375691aea3be12
yooodleee <accia25@naver.com>
2025-01-21 14:15:33 +0900

    reset age and increment all to move age

--- Alphago/dlgo/utils.py
+++ Alphago/dlgo/utils.py
@@ -43,3 +43,24 @@ def coords_from_point(point):
     )
 
 
+# this feature is only used in goboard_fast.py
+class MoveAge():
+    def __init__(self, board):
+        self.move_ages = - np.ones(
+            (
+                board.num_rows,
+                board.num_cols,
+            )
+        )
+    
+    def get(self, row, col):
+        return self.move_ages[row, col]
+    
+    def reset_age(self, point):
+        self.move_ages[point.row - 1, point.col - 1] = -1
+    
+    def add(self, point):
+        self.move_ages[point.row - 1, point.col - 1] = 0
+    
+    def increment_all(self):
+        self.move_ages[self.move_ages > -1] += 1
