d16e4f3e27f25294580a736d5abbe2da05244a6b
yooodleee <accia25@naver.com>
2025-01-22 11:05:00 +0900

    create __init__.py

---
+++ Alphago/dlgo/encoders/__init__.py
@@ -0,0 +1,5 @@
+from dlgo.encoders.alphago import *
+from dlgo.encoders.base import *
+from dlgo.encoders.oneplane import *
+from dlgo.encoders.sevenplane import *
+from dlgo.encoders.simple import *
