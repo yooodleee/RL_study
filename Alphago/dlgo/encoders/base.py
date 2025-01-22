6176211dc1995da48e9837fcfdba0b3479d30e75
yooodleee <accia25@naver.com>
2025-01-22 11:30:26 +0900

    get encoder by name

--- Alphago/dlgo/encoders/base.py
+++ Alphago/dlgo/encoders/base.py
@@ -26,3 +26,9 @@ class Encoder:
         raise NotImplementedError
 
 
+def get_encoder_by_name(name, board_size):
+    if isinstance(board_size, int):
+        board_size = (board_size, board_size)
+    module = importlib.import_module('dlgo.encoders.' + name)
+    constructor = getattr(module, 'create')
+    return constructor(board_size)
