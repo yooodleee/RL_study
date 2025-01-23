bdc560fb2ba75fdc76e61ccc86f842eac3bb311c
yooodleee <accia25@naver.com>
2025-01-23 17:15:35 +0900

    gtp_position_to_coords

--- Alphago/dlgo/gtp/board.py
+++ Alphago/dlgo/gtp/board.py
@@ -10,3 +10,7 @@ def coords_to_gtp_position(move):
     return COLS[point.col - 1] + str(point.move)
 
 
+def gtp_position_to_coords(gtp_position):
+    col_str, row_str = gtp_position[0], gtp_position[1:]
+    point = Point(int(row_str), COLS.find(col_str.upper()) + 1)
+    return Move(point)
