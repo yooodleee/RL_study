c9f766c08cda03e7dc529041d73d568ad542766d
yooodleee <accia25@naver.com>
2025-01-23 19:12:57 +0900

    compare result and bestsofar return best_result

--- Alphago/dlgo/minmax/depthprune.py
+++ Alphago/dlgo/minmax/depthprune.py
@@ -20,3 +20,33 @@ def reserve_game_result(game_result):
     return GameResult.draw
 
 
+def best_result(
+        game_state,
+        max_depth,
+        eval_fn):
+    
+    if game_state.is_over():
+        if game_state.winner() == game_state.next_player:
+            return MAX_SCORE
+        else:
+            return MIN_SCORE
+    
+    if max_depth == 0:
+        return eval_fn(game_state)
+    
+    best_so_far = MIN_SCORE
+    for candidate_move in game_state.legal_moves():
+        next_state = game_state.apply_move(candidate_move)
+        opponent_best_result = best_result(
+            next_state,
+            max_depth,
+            -1,
+            eval_fn,
+        )
+        our_result = -1 * opponent_best_result
+        if our_result > best_so_far:
+            best_so_far = our_result
+    
+    return best_so_far
+
+
