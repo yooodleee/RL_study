661cff3a293619da7e9d569f5ee32ce68607a587
yooodleee <accia25@naver.com>
2025-01-23 21:26:03 +0900

    add ActivationLayer

--- Alphago/dlgo/nn/layers.py
+++ Alphago/dlgo/nn/layers.py
@@ -48,3 +48,24 @@ class Layer:
         raise NotImplementedError
 
 
+class ActivationLayer(Layer):
+    def __init__(self, input_dim):
+        super(ActivationLayer, self).__init__()
+
+        self.input_dim = input_dim
+        self.output_dim = input_dim
+    
+    def forward(self):
+        data = self.get_forward_input()
+        self.output_data = sigmoid(data)
+    
+    def backward(self):
+        delta = self.get_backward_input()
+        data = self.get_forward_input()
+        self.output_delta = delta * sigmoid_prime(data)
+    
+    def describe(self):
+        print("|-- " + self.__class__.__name__)
+        print(" |-- dimensions: ({}, {})".format(self.input_dim, self.output_dim))
+
+
