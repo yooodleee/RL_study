09e595953d3c3618cc3ede7889ff0526b593e647
yooodleee <accia25@naver.com>
2025-01-23 21:01:48 +0900

    return average digit

---
+++ Alphago/dlgo/nn/average_digits.py
@@ -0,0 +1,11 @@
+import numpy as np
+from load_mnist import load_data
+from layers import sigmoid_double
+
+
+def average_digit(data, digit):
+    filtered_data = [x[0] for x in data if np.argmax(x[1]) == digit]
+    filtered_array = np.asarray(filtered_data)
+    return np.average(filtered_array, axis=0)
+
+
