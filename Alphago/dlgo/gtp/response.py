6517e39e6c410387066ea7c19a798dfbaa6e42f9
yooodleee <accia25@naver.com>
2025-01-23 18:13:16 +0900

    serialize gtp's command and response

--- Alphago/dlgo/gtp/response.py
+++ Alphago/dlgo/gtp/response.py
@@ -16,3 +16,9 @@ def bool_response(boolean):
     return success('true') if boolean is True else success('false')
 
 
+def serialize(gtp_command, gtp_response):
+    return '{}{} {}\n\n'.format(
+        '=' if gtp_response.success else '?',
+        '' if gtp_command.sequence is None else str(gtp_command.sequence),
+        gtp_response.body,
+    )
