9882ddcf82e940a94b211858e3d193de696d9a60
yooodleee <accia25@naver.com>
2025-01-23 19:04:47 +0900

    add AlphaBetaAgent

--- Alphago/dlgo/minmax/alphabeta.py
+++ Alphago/dlgo/minmax/alphabeta.py
@@ -60,3 +60,35 @@ def alpha_beta_result(
     return best_so_far
 
 
+class AlphaBetaAgent(Agent):
+    def __init__(self, max_depth, eval_fn):
+        Agent.__init__(self)
+        self.max_depth = max_depth
+        self.eval_fn = eval_fn
+    
+    def select_move(self, game_state):
+        best_moves = []
+        best_score = None
+        best_black = MIN_SCORE
+        best_white = MIN_SCORE
+        for possible_move in game_state.legal_moves():
+            next_state = game_state.apply_move(possible_move)
+            opponent_best_outcome = alpha_beta_result(
+                next_state,
+                self.max_depth,
+                best_black,
+                best_white,
+                self.eval_fn,
+            )
+            our_best_outcome = -1 * opponent_best_outcome
+            if (not best_moves) or our_best_outcome > best_score:
+                best_moves = [possible_move]
+                best_score = our_best_outcome
+                if game_state.next_player == Player.black:
+                    best_black = best_score
+                elif game_state.next_player == Player.white:
+                    best_white = best_score
+            elif our_best_outcome == best_score:
+                best_moves.append(possible_move)
+                
+        return random.choice(best_moves)
