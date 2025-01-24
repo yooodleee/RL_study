7f54e9a6e4355252304cca7bb4dc77de10547ceb
yooodleee <accia25@naver.com>
2025-01-25 00:28:19 +0900

    fix import depthprunedAgent correctly

--- Alphago/pruned_go.py
+++ Alphago/pruned_go.py
@@ -2,7 +2,7 @@ from six.moves import input
 
 from dlgo import goboard_fast as goboard
 from dlgo import gotypes
-from dlgo import minmax
+from dlgo.minmax import depthprune
 from dlgo.utils import print_board, print_move, point_from_coords
 
 
@@ -27,3 +27,21 @@ def capture_diff(game_state):
     return -1 * diff
 
 
+def main():
+    game = goboard.GameState.new_game(BOARD_SIZE)
+    bot = depthprune.DepthPrunedAgent(3, capture_diff)
+
+    while not game.is_over():
+        print_board(game.board)
+        if game.next_player == gotypes.Player.black:
+            human_move = input('-- ')
+            point = point_from_coords(human_move.strip())
+            move = goboard.Move.play(point)
+        else:
+            move = bot.select_move(game)
+        print_move(game.next_player, move)
+        game = game.apply_move(move)
+
+
+if __name__ == '__main__':
+    main()
