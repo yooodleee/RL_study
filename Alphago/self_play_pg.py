141ac221b11018d0fda7417fa38c4c9df1f29062
yooodleee <accia25@naver.com>
2025-01-25 00:44:05 +0900

    simulate the game

--- Alphago/self_play_pg.py
+++ Alphago/self_play_pg.py
@@ -12,3 +12,21 @@ class GameRecord(namedtuple('GameRecord', 'Winner')):
     pass
 
 
+def simulate_game(black_player, white_player):
+    game = GameState.new_game(BOARD_SIZE)
+    agents = {
+        Player.black: black_player,
+        Player.white: white_player,
+    }
+    while not game.is_over():
+        next_move = agents[game.next_player].select_move(game)
+        game = game.apply_move(next_move)
+    game_result = scoring.compute_game_result(game)
+
+    print(game_result)
+    
+    return GameRecord(
+        Winner=game_result.winner
+    )
+
+
