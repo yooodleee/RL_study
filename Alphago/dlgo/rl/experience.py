a7f4d1e121fbe46e948b5136fe94b6214b61e47a
yooodleee <accia25@naver.com>
2025-01-24 10:08:01 +0900

    load experience

--- Alphago/dlgo/rl/experience.py
+++ Alphago/dlgo/rl/experience.py
@@ -101,3 +101,10 @@ def combine_experience(collectors):
     )
 
 
+def load_experience(h5file):
+    return ExperienceBuffer(
+        states=np.array(h5file['experience']['states']),
+        actions=np.array(h5file['experience']['actions']),
+        rewards=np.array(h5file['experience']['rewards']),
+        advantages=np.array(h5file['experience']['advantages']),
+    )
