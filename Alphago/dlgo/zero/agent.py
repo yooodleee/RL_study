ba5a266b9ea6062e448a67610ce211555d338ac6
yooodleee <accia25@naver.com>
2025-01-24 11:22:50 +0900

    add ZeroTreeNode

--- Alphago/dlgo/zero/agent.py
+++ Alphago/dlgo/zero/agent.py
@@ -11,3 +11,53 @@ class Branch:
         self.total_value = 0.0
 
 
+class ZeroTreeNode:
+    def __init__(
+            self,
+            state,
+            value,
+            priors,
+            parent,
+            last_move):
+        
+        self.state = state
+        self.value = value
+        self.parent = parent
+        self.last_move = last_move
+        self.total_visit_count = 1
+        self.branches = {}
+        for move, p in priors.items():
+            if state.is_valid_move(move):
+                self.branches[move] = Branch(p)
+        self.children = {}
+    
+    def moves(self):
+        return self.branches.keys()
+    
+    def add_child(self, move, child_node):
+        self.children[move] = child_node
+    
+    def has_child(self, move):
+        return move in self.children
+    
+    def record_visit(self, move, value):
+        self.total_visit_count += 1
+        self.branches[move].visit_count += 1
+        self.branches[move].total_value += value
+    
+    def expected_value(self, move):
+        branch = self.branches[move]
+        if branch.visit_count == 0:
+            return 0.0
+        return branch.total_value / branch.visit_count
+    
+    def prior(self, move):
+        return self.branches[move].prior
+    
+    def visit_count(self, move):
+        if move in self.branches:
+            return self.branches[move].visit_count
+        
+        return 0
+
+
