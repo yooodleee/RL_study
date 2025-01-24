19271836b4bea19e5550f14ff5c8b8be6371cd24
yooodleee <accia25@naver.com>
2025-01-24 14:20:35 +0900

    print board and move in main

--- Alphago/alpha_beta_go.py
+++ Alphago/alpha_beta_go.py
@@ -26,3 +26,21 @@ def capture_diff(game_state):
     return -1 * diff
 
 
+def main():
+    game = goboard.GameState.new_game(BOARD_SIZE)
+    bot = minmax.AlphaBetaAgent(3, capture_diff)
+
+    while not game.is_over():
+        print_board(game.board)
+        if game.next_player == gotypes.Player.black:
+            human_move = input('-- ')
+            point = point_from_coords(human_move.strip())
+            move = goboard.Move.play(point)
+        else:
+            move = bot.select_move(game)
+        print_move(game.next_player, move)
+        game = game.apply_move(move)
+
+
+if __name__ == '__main__':
+    main()
