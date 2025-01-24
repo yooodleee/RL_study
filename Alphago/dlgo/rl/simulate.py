b229f2a3475125e9245b64459ec8e5175734fe8e
yooodleee <accia25@naver.com>
2025-01-24 10:30:57 +0900

    experience_simulation

--- Alphago/dlgo/rl/simulate.py
+++ Alphago/dlgo/rl/simulate.py
@@ -31,3 +31,29 @@ def simulate_game(black_player, white_player):
     )
 
 
+def experience_simulation(num_games, agent1, agent2):
+    collector1 = rl.ExperienceCollector()
+    collector2 = rl.ExperienceCollector()
+
+    color1 = Player.black
+    for i in range(num_games):
+        collector1.begin_episode()
+        agent1.set_collector(collector1)
+        collector2.begin_episode()
+        agent2.set_collector(collector2)
+
+        if color1 == Player.black:
+            black_player, white_player = agent1, agent2
+        else:
+            white_player, black_player = agent2, agent1
+
+        game_record = simulate_game(black_player, white_player)
+        if game_record.winner == color1:
+            collector1.complete_episode(reward=1)
+            collector2.complete_episode(reward=-1)
+        else:
+            collector2.complete_episode(reward=1)
+            collector1.complete_episode(reward=-1)
+        color1 = color1.other
+    
+    return rl.combine_experience([collector1, collector2])
