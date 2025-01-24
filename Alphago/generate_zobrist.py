113968da290bcdc82b6f2294b313492d1c90ef6c
yooodleee <accia25@naver.com>
2025-01-24 16:11:32 +0900

    print board state

--- Alphago/generate_zobrist.py
+++ Alphago/generate_zobrist.py
@@ -10,3 +10,24 @@ def to_python(player_state):
     return Player.white
 
 
+MAX63 = 0x7fffffffffffffff
+
+table = {}
+empty_board = 0
+for row in range(1, 20):
+    for col in range(1, 20):
+        for state in (Player.black, Player.white, None):
+            code = random.randint(0, MAX63)
+            table[Point(row, col), state] = code
+
+
+print('from .gotypes import Player, Point')
+print('')
+print("__all__ = ['HASH_CODE', 'EMPTY_BOARD']")
+print('')
+print('HASH_CODE = {')
+for (pt, state), hash_code in table.items():
+    print('     (%r, %s): %r,' % (pt, to_python(state), hash_code))
+print('}')
+print('')
+print('EMPTY_BOARD = %d' % (empty_board))
