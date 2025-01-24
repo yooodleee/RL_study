1b4e2b456f6b506fc4f9a7e909000647924dd029
yooodleee <accia25@naver.com>
2025-01-25 01:09:18 +0900

    create sgf_to_game_state.py

---
+++ Alphago/sgf_to_game_state.py
@@ -0,0 +1,23 @@
+from dlgo.gosgf import Sgf_game
+from dlgo.goboard_fast import GameState, Move
+from dlgo.gotypes import Point
+from dlgo.utils import print_board
+
+
+sgf_content = "(;GM[1]FF[4]SZ[9];B[ee];W[ef];B[ff])" + \
+                ";W[df];B[fe];W[fc];B[ec];W[gd];B[fb])"
+
+sgf_game = Sgf_game.from_string(sgf_content)
+
+
+game_state = GameState.new_game(19)
+
+
+for item in sgf_game.main_sequence_iter():
+    color, move_tupe = item.get_move()
+    if color is not None and move_tupe is not None:
+        row, col = move_tupe
+        point = Point(row + 1, col + 1)
+        move = Move.play(point)
+        game_state = game_state.apply_move(move)
+        print_board(game_state.board)
