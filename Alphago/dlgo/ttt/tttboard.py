ab4df2824f52bd9ff96326c33eaabf6624d34f90
yooodleee <accia25@naver.com>
2025-01-24 11:08:26 +0900

    add Board

--- Alphago/dlgo/ttt/tttboard.py
+++ Alphago/dlgo/ttt/tttboard.py
@@ -21,3 +21,21 @@ DIAG_1 = (Point(1, 1), Point(2, 2), Point(3, 3))
 DIAG_2 = (Point(1, 3), Point(2, 2), Point(3, 1))
 
 
+class Board:
+    def __init__(self):
+        self._grid = {}
+    
+    def place(self, player, point):
+        assert self.is_on_grid(point)
+        assert self._grid.get(point) is None
+        self._grid[point] = player
+    
+    @staticmethod
+    def is_on_grid(point):
+        return 1 <= point.row <= BOARD_SIZE and \
+            1 <= point.col <= BOARD_SIZE
+    
+    def get(self, point):
+        return self._grid.get(point)
+
+
