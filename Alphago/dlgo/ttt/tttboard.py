88b145acceaf80b0f851101471083c9cda205ca6
yooodleee <accia25@naver.com>
2025-01-24 11:06:47 +0900

    board size for rows, cols, diag1, diag2

--- Alphago/dlgo/ttt/tttboard.py
+++ Alphago/dlgo/ttt/tttboard.py
@@ -14,3 +14,10 @@ class IllegalMoveError(Exception):
     pass
 
 
+BOARD_SIZE = 3
+ROWS = tuple(range(1, BOARD_SIZE + 1))
+COLS = tuple(range(1, BOARD_SIZE + 1))
+DIAG_1 = (Point(1, 1), Point(2, 2), Point(3, 3))
+DIAG_2 = (Point(1, 3), Point(2, 2), Point(3, 1))
+
+
