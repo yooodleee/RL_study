03a6bbc2183b45b622670ac4646e035979f4669d
yooodleee <accia25@naver.com>
2025-01-24 15:23:55 +0900

    main for eval_ac_bot.py

--- Alphago/eval_ac_bot.py
+++ Alphago/eval_ac_bot.py
@@ -33,3 +33,42 @@ def simulate_game(black_player, white_player):
     )
 
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--agent1', required=True
+    )
+    parser.add_argument(
+        '--agent2', required=True
+    )
+    parser.add_argument(
+        '--num-games', '-n', type=int, default=10
+    )
+
+    args = parser.parse_args()
+    agent1 = rl.load_ac_agent(h5py.File(args.agent1))
+    agent2 = rl.load_ac_agent(h5py.File(args.agent2))
+    num_games = args.num_games
+
+    wins = 0
+    losses = 0
+    color1 = Player.black
+    for i in range(num_games):
+        print('Simulating game %d%d...' % (i + 1, num_games))
+        if color1 == Player.black:
+            black_player, white_player = agent1, agent2
+        else:
+            white_player, black_player = agent1, agent2
+        game_record = simulate_game(
+            black_player, white_player
+        )
+        if game_record.winner == color1:
+            wins += 1
+        else:
+            losses += 1
+        color1 = color1.other
+    print('Agent 1 record: %d%d' % (wins, wins + losses))
+
+
+if __name__ == '__main__':
+    main()
