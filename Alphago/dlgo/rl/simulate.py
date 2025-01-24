0a5ad4ee1c1aae59355661dd485461705a2c180c
yooodleee <accia25@naver.com>
2025-01-24 10:27:27 +0900

    simulate_game

--- Alphago/dlgo/rl/simulate.py
+++ Alphago/dlgo/rl/simulate.py
@@ -10,3 +10,24 @@ class GameRecord(namedtuple('GameRecord', 'moves winner')):
     pass
 
 
+def simulate_game(black_player, white_player):
+    moves = []
+    game = goboard.GameState.new_game(19)
+    agents = {
+        Player.black: black_player,
+        Player.white: white_player,
+    }
+    while not game.is_over():
+        next_move = agents[game.next_player].select_move(game)
+        moves.append(next_move)
+        game = game.apply_move(next_move)
+    
+    game_result = scoring.compute_game_result(game)
+    print(game_result)
+
+    return GameRecord(
+        moves=moves,
+        winner=game_result.winner,
+    )
+
+
