d82b8f41de1bba4d09b4c632c3886314c50dedb4
yooodleee <accia25@naver.com>
2025-01-24 14:16:36 +0900

    capture_diff

---
+++ Alphago/alpha_beta_go.py
@@ -0,0 +1,28 @@
+from six.moves import input
+
+from dlgo import goboard_fast as goboard
+from dlgo import gotypes
+from dlgo import minmax
+from dlgo.utils import print_board, print_move, point_from_coords
+
+
+BOARD_SIZE = 5
+
+
+def capture_diff(game_state):
+    black_stones = 0
+    white_stones = 0
+    for r in range(1, game_state.board.num_rows + 1):
+        for c in range(1, game_state.board.num_cols + 1):
+            p = gotypes.Point(r, c)
+            color = game_state.board.get(p)
+            if color == gotypes.Player.black:
+                black_stones += 1
+            elif color == gotypes.Player.white:
+                white_stones += 1
+    diff = black_stones - white_stones
+    if game_state.next_player == gotypes.Player.black:
+        return diff
+    return -1 * diff
+
+
