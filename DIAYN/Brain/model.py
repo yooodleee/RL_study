403711ebe408f56523d674cfd66b64d6f45e956e
yooodleee <accia25@naver.com>
2025-01-26 18:40:45 +0900

    add QvalueNetwork

--- DIAYN/Brain/model.py
+++ DIAYN/Brain/model.py
@@ -67,3 +67,32 @@ class ValueNetwork(nn.Module, ABC):
         return self.value(x)
     
 
+class QvalueNetwork(nn.Module, ABC):
+    def __init__(
+            self,
+            n_states,
+            n_actions,
+            n_hidden_filters=256):
+        
+        super(QvalueNetwork, self).__init__()
+        self.n_states = n_states
+        self.n_hidden_filters = n_hidden_filters
+        self.n_actions = n_actions
+
+        self.hidden1 = nn.Linear(in_features=self.n_states + self.n_actions, out_features=self.n_hidden_filters)
+        init_weight(self.hidden1)
+        self.hidden1.bias.data.zero_()
+        self.hidden2 = nn.Linear(in_features=self.n_hidden_filters, out_features=self.n_hidden_filters)
+        init_weight(self.hidden2)
+        self.hidden2.bias.data.zero_()
+        self.q_value = nn.Linear(in_features=self.n_hidden_filters, out_features=1)
+        init_weight(self.q_value, initializer="xavier uniform")
+        self.q_value.bias.data.zero_()
+
+    def forward(self, states, actions):
+        x = torch.cat([states, actions], dim=1)
+        x = F.relu(self.hidden1(x))
+        x = F.relu(self.hidden2(x))
+        return self.q_value(x)
+    
+
