297484a371f55ea677cad02d5ec267e4f481ae4d
yooodleee <accia25@naver.com>
2025-01-26 18:33:27 +0900

    add Discriminator

--- DIAYN/Brain/model.py
+++ DIAYN/Brain/model.py
@@ -12,3 +12,32 @@ def init_weight(layer, initializer="he normal"):
         nn.init.kaiming_normal_(layer.weight)
 
 
+class Discriminator(nn.Module, ABC):
+    def __init__(
+            self,
+            n_states,
+            n_skills,
+            n_hidden_filters=256):
+        
+        super(Discriminator, self).__init__()
+        self.n_states = n_states
+        self.n_skills = n_skills
+        self.n_hidden_filters = n_hidden_filters
+
+        self.hidden1 = nn.Linear(in_features=self.n_states, out_features=self.n_hidden_filters)
+        init_weight(self.hidden1)
+        self.hidden1.bias.data.zero_()
+        self.hidden2 = nn.Linear(in_features=self.n_hidden_filters, out_features=self.n_hidden_filters)
+        init_weight(self.hidden2)
+        self.hidden2.bias.zero_()
+        self.q = nn.Linear(in_features=self.n_hidden_filters, out_features=self.n_skills)
+        init_weight(self.q, initializer="xavier uniform")
+        self.q.bias.data.zero_()
+
+    def forward(self, states):
+        x = F.relu(self.hidden1(states))
+        x = F.relu(self.hidden2(x))
+        logits = self.q(x)
+        return logits
+    
+
