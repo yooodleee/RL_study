3ac02008c6b84bcc24a5a3fdeb0e4496ed1a4441
yooodleee <accia25@naver.com>
2025-01-26 19:06:17 +0900

    add PolicyNetwork

--- DIAYN/Brain/model.py
+++ DIAYN/Brain/model.py
@@ -96,3 +96,54 @@ class QvalueNetwork(nn.Module, ABC):
         return self.q_value(x)
     
 
+class PolicyNetwork(nn.Module, ABC):
+    def __init__(
+            self,
+            n_states,
+            n_actions,
+            action_bounds,
+            n_hidden_filters=256):
+        
+        super(PolicyNetwork, self).__init__()
+        self.n_states = n_states
+        self.n_hidden_filters=n_hidden_filters
+        self.n_actions = n_actions
+        self.action_bounds = action_bounds
+
+        self.hidden1 = nn.Linear(in_features=self.n_states, out_features=self.n_hidden_filters)
+        init_weight(self.hidden1)
+        self.hidden1.bias.data.zero_()
+        self.hidden2 = nn.Linear(in_features=self.n_hidden_filters, out_features=self.n_hidden_filters)
+        init_weight(self.hidden2)
+        self.hidden2.bias.data.zero_()
+        
+        self.mu = nn.Linear(in_features=self.n_hidden_filters, out_features=n_actions)
+        init_weight(self.mu, initializer="xavier uniform")
+        self.mu.bias.data.zero_()
+
+        self.log_std = nn.Linear(in_features=self.n_hidden_filters, out_features=self.n_actions)
+        init_weight(self.log_std, initializer="xavier uniform")
+        self.log_std.bias.data.zero_()
+
+    def forward(self, states):
+        x = F.relu(self.hidden1(states))
+        x = F.relu(self.hidden2(x))
+
+        mu = self.mu(x)
+        log_std = self.log_std(x)
+        std = log_std.clamp(min=-20, max=2).exp()
+        dist = Normal(mu, std)
+        return dist
+    
+    def sample_or_likelihood(self, states):
+        dist = self(states)
+        # Reparameterization trick
+        u = dist.resample()
+        action = torch.tanh(u)
+        log_prob = dist.log_prob(value=u)
+        # Enforcing action bounds
+        log_prob -= torch.log(1 - action ** 2 + 1e-6)
+        log_prob = log_prob.sum(-1, keepdim=True)
+        return (
+            action * self.action_bounds[1]
+        ).clamp_(self.action_bounds[0], self.action_bounds[1]), log_prob
