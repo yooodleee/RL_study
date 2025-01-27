a9abbdc97a58132fe7635bfbdc8a59478513617b
yooodleee <accia25@naver.com>
2025-01-27 12:33:29 +0900

    init weights for output layers

--- Meta_RL/helper.py
+++ Meta_RL/helper.py
@@ -33,3 +33,10 @@ def discount(x, gamma):
     return scipy.signal.lfilter([1], [1 - gamma], x[::-1], axis=0)[::-1]
 
 
+# Used to initialize weights for policy and value output layers
+def normalized_columns_initializer(std=1.0):
+    def _initializer(shape, dtype=None, partition_info=None):
+        out = np.random.randn(*shape).astype(np.float32)
+        out *= std / np.sqrt(np.square(out).sum(axis=0, keepdims=True))
+        return tf.constant(out)
+    return _initializer
