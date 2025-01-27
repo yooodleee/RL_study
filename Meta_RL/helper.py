a04be48b1c6682c85fd0a04acf832d55e2cc20ba
yooodleee <accia25@naver.com>
2025-01-27 12:31:12 +0900

    discounting function to calc discount returns

--- Meta_RL/helper.py
+++ Meta_RL/helper.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import scipy.signal
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import scipy.misc
@@ -25,5 +26,10 @@ def update_target_graph(from_scope, to_scope):
     for from_var, to_var in zip(from_vars, to_vars):
         op_holder.append(to_var.assign(from_var))
     return op_holder
+
+
+# Discounting function used to calculate discounted returns.
+def discount(x, gamma):
+    return scipy.signal.lfilter([1], [1 - gamma], x[::-1], axis=0)[::-1]
 
 
