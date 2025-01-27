c0e7601e49d5d8de66e5e11378eafc203ae86d7e
yooodleee <accia25@naver.com>
2025-01-27 20:59:11 +0900

    add HalfCheetahEnv

--- METRPO/envs/com_half_cheetah_env.py
+++ METRPO/envs/com_half_cheetah_env.py
@@ -11,3 +11,13 @@ def smooth_abs(x, param):
     return np.sqrt(np.square(x) + np.square(param)) - param
 
 
+class HalfCheetahEnv(MujocoEnv, Serializable):
+
+    FILE = 'half_cheetah.xml'
+
+    def __init__(self, *args, **kwargs):
+        super(HalfCheetahEnv, self).__init__(*args, **kwargs)
+        Serializable.__init__(self, *args, **kwargs)
+        self.ctrl_cost_coeff = 1e-1
+    
+    
