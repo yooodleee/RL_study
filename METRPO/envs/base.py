56528357555046dae97d425031a53c411cedeaea
yooodleee <accia25@naver.com>
2025-01-27 20:28:02 +0900

    add VecTfEnv

--- METRPO/envs/base.py
+++ METRPO/envs/base.py
@@ -38,7 +38,7 @@ class WrappedCls(object):
 
 
 class TfEnv(ProxyEnv):
-    
+
     @cached_property
     def observation_space(self):
         return to_tf_space(self.wrapped_env.observation_space)
@@ -71,3 +71,20 @@ class TfEnv(ProxyEnv):
         return WrappedCls(cls, env_cls, extra_kwargs)
 
 
+class VecTfEnv(object):
+
+    def __init__(self, vec_env):
+        self.vec_env = vec_env
+    
+    def reset(self):
+        return self.vec_env.reset()
+    
+    @property
+    def num_envs(self):
+        return self.vec_env.num_envs
+    
+    def step(self, action_n):
+        return self.vec_env.step(action_n)
+    
+    def terminate(self):
+        self.vec_env.terminate()
