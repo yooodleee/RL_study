d4317b846780f702bc38ce50ea745f73706cba14
yooodleee <accia25@naver.com>
2025-01-28 14:31:23 +0900

    ob, reward, done for _step

--- METRPO/envs/walker_env.py
+++ METRPO/envs/walker_env.py
@@ -9,4 +9,19 @@ class Walker2dEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         mujoco_env.MujocoEnv.__init__(self, "walker2d.xml", 4)
         utils.EzPickle.__init__(self)
 
+    def _step(self, a):
+        posbefore = self.model.data.qpos[0, 0]
+        self.do_simulation(a, self.frame_skip)
+        posafter, height, ang = self.model.data.qpos[0:3, 0]
+        alive_bonus = 1.0
+        reward = ((posafter - posbefore) / self.dt)
+        reward += alive_bonus
+        reward -= 1e-3 * np.square(a).sum()
+        done = not (
+            height > 0.8 and height < 2.0 and
+            ang > -1.0 and ang < 1.0
+        )
+        ob = self._get_obs()
+        return ob, reward, done, {}
+    
     
