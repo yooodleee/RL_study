e0f3cd652bd600eca7dabf5c91d628a7ec182717
yooodleee <accia25@naver.com>
2025-01-28 14:29:03 +0900

    add Walker2dEnv

---
+++ METRPO/envs/walker_env.py
@@ -0,0 +1,12 @@
+import numpy as np
+from gym import utils
+from gym.envs.mujoco import mujoco_env
+
+
+class Walker2dEnv(mujoco_env.MujocoEnv, utils.EzPickle):
+
+    def __init__(self):
+        mujoco_env.MujocoEnv.__init__(self, "walker2d.xml", 4)
+        utils.EzPickle.__init__(self)
+
+    
