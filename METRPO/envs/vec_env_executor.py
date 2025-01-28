1c3e4ed253454f0f4dd9786455ffb57802f7a9d0
yooodleee <accia25@naver.com>
2025-01-28 14:09:18 +0900

    obs, rewards, dones for step

--- METRPO/envs/vec_env_executor.py
+++ METRPO/envs/vec_env_executor.py
@@ -16,4 +16,18 @@ class VecEnvExecutor(object):
         self.ts = np.zeros(len(self.envs), dtype='int')
         self.max_path_length = max_path_length
     
+    def step(self, action_n):
+        all_results = [env.step(a) for (a, env) in zip(action_n, self.envs)]
+        obs, rewards, dones, env_infos = list(map(list, list(zip(*all_results))))
+        dones = np.asarray(dones)
+        rewards = np.asarray(rewards)
+        self.ts += 1
+        if self.max_path_length is not None:
+            dones[self.ts >= self.max_path_length] = True
+        for (i, done) in enumerate(dones):
+            if done:
+                obs[i] = self.envs[i].reset()
+                self.ts[i] = 0
+        return obs, rewards, dones, tensor_utils.stack_tensor_dict_list(env_infos)
+    
     
