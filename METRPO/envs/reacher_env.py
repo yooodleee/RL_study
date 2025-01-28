63d9d45d97c93ad2e9283ac06b7d7279958b1488
yooodleee <accia25@naver.com>
2025-01-28 13:52:22 +0900

    tf for cost

--- METRPO/envs/reacher_env.py
+++ METRPO/envs/reacher_env.py
@@ -119,3 +119,14 @@ class ReacherEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         return np.mean(np.linalg.norm(x[:, -2:] - get_fingertips(x), axis=1) \
                        + ctrl_cost_coeff * 0.5 * np.sum(np.square(u), axis=1))
     
+    def cost_tf(
+            self,
+            x,
+            u,
+            x_next,
+            ctrl_cost_coeff=2):
+        
+        return tf.reduce_mean(tf.norm(x[:, -2:]-get_fingertips_tf(x), axis=1) \
+                              + ctrl_cost_coeff*0.5*tf.reduce_sum(tf.square(u), axis=1))
+    
+    
