978def890c636c791a71c7e04ac7a11c92464cf3
yooodleee <accia25@naver.com>
2025-01-28 14:37:54 +0900

    np for cost

--- METRPO/envs/walker_env.py
+++ METRPO/envs/walker_env.py
@@ -56,4 +56,14 @@ class Walker2dEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         """
         return 4
     
+    def cost_np(
+            self,
+            x,
+            u,
+            x_next,
+            ctrl_cost_coeff=2):
+        
+        assert np.amax(np.abs(u)) <= 1.0
+        return np.mean(np.linalg.norm(x[:, -2:] - get_fingertips(x), axis=1) \
+                       + ctrl_cost_coeff * 0.5 * np.sum(np.square(u), axis=1))
     
