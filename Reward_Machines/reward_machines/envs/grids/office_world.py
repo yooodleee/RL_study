f14276bbe5b24a8272e335641d35df938da8c823
yooodleee <accia25@naver.com>
2025-01-28 17:07:44 +0900

    update OfficeWorld

--- Reward_Machines/reward_machines/envs/grids/office_world.py
+++ Reward_Machines/reward_machines/envs/grids/office_world.py
@@ -11,4 +11,25 @@ class OfficeWorld:
         self._load_map()
         self.map_height, self.map_width = 12, 9
     
+    def reset(self):
+        self.agent = (2, 1)
+    
+    def execute_action(self, a):
+        """
+        execute 'action' in the game.
+        """
+        x, y = self.agent
+        self.agent = self._get_new_position(x, y, a)
+
+    def _get_new_position(self, x, y, a):
+        action = Actions(a)
+        # executing action
+        if (x, y, action) not in self.forbidden_transitions:
+            if action == Actions.up     : y += 1
+            if action == Actions.down   : y -= 1
+            if action == Actions.left   : x -= 1
+            if action == Actions.right  : x += 1
+    
+        return x, y
+    
     
