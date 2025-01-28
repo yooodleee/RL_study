7779b198fab1ab9c5a9959f8386057a62fcb0ca6
yooodleee <accia25@naver.com>
2025-01-28 15:58:34 +0900

    get agent's next position

--- Reward_Machines/reward_machines/envs/grids/craft_world.py
+++ Reward_Machines/reward_machines/envs/grids/craft_world.py
@@ -32,4 +32,17 @@ class CraftWorld:
         if action_succeeded:
             agent.change_position(ni, nj)
     
+    def _get_next_position(self, ni, nj, a):
+        """
+        Returns the position where the agent would be if we execute action.
+        """
+        action = Actions(a)
+
+        # OBS: Invalid actions behave as NO-OP
+        if action == Actions.up     : ni -= 1
+        if action == Actions.down   : ni += 1
+        if action == Actions.left   : nj -= 1
+        if action == Actions.right  : nj += 1
+
+        return ni, nj
     
