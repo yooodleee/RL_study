1851d5ccc3750dc77c12a4f2515a6c4bb84f1323
yooodleee <accia25@naver.com>
2025-01-28 15:56:10 +0900

    execute action in the game

--- Reward_Machines/reward_machines/envs/grids/craft_world.py
+++ Reward_Machines/reward_machines/envs/grids/craft_world.py
@@ -14,4 +14,22 @@ class CraftWorld:
     def reset(self):
         self.agent.reset()
     
+    def execute_action(self, a):
+        """
+        execute 'action' in the game.
+        """
+        agent = self.agent
+        ni, nj = agent.i, agent.j
+
+        # Getting new position after executing action
+        ni, nj = self._get_next_position(ni, nj, a)
+
+        # Interacting with the objects that is in the next position
+        # (this doesn't include monsters)
+        action_succeeded = self.map_array[ni][nj].interact(agent)
+
+        # So far, an action can only fail if the new position is a wall
+        if action_succeeded:
+            agent.change_position(ni, nj)
+    
     
