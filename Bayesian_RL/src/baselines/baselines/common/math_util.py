020b4567c520bfe01ca3c9c0a899d185fe8c5059
yooodleee <accia25@naver.com>
2025-01-29 21:40:26 +0900

    test discount with boundaries

--- Bayesian_RL/src/baselines/baselines/common/math_util.py
+++ Bayesian_RL/src/baselines/baselines/common/math_util.py
@@ -82,3 +82,14 @@ def discount_with_boundaries(X, New, gamma):
     return Y
 
 
+def test_discount_with_boundaries():
+    gamma = 0.9
+    x = np.array([1.0, 2.0, 3.0, 4.0], 'float32')
+    starts = [1.0, 0.0, 0.0, 1.0]
+    y = discount_with_boundaries(x, starts, gamma)
+    assert np.allclose(y, [
+        1 + gamma * 2 + gamma ** 2 * 3,
+        2 + gamma * 3,
+        3,
+        4,
+    ])
