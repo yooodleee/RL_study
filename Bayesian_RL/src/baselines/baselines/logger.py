891b4f1e60126085df2ff2b3ca16e94d970cc2cb
yooodleee <accia25@naver.com>
2025-01-29 16:40:10 +0900

    summary value and write kvs

--- Bayesian_RL/src/baselines/baselines/logger.py
+++ Bayesian_RL/src/baselines/baselines/logger.py
@@ -166,4 +166,19 @@ class TensorBoardOutputFormat(KVWriter):
         self.pywrap_tensorflow = pywrap_tensorflow
         self.writer = pywrap_tensorflow.EventWriter(compat.as_bytes(path))
     
+    def writekvs(self, kvs):
+        def summary_val(k, v):
+            kwargs = {'tag': k, 'simple_value': float(v)}
+            return self.tf.Summary.Value(**kwargs)
+        summary = self.tf.Summary(
+            value=[summary_val(k, v) for k, v in kvs.items()]
+        )
+        event = self.event_pb2.Event(
+            wall_time=time.time(), summary=summary
+        )
+        event.step = self.step  # is there any reason why you'd want to specify the step?
+        self.writer.WriteEvent(event)
+        self.writer.Flush()
+        self.step += 1
+    
     
