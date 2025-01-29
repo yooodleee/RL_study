5c4174663fc11d464e850624e0f323277df721b8
yooodleee <accia25@naver.com>
2025-01-29 16:31:59 +0900

    write current row to the history

--- Bayesian_RL/src/baselines/baselines/logger.py
+++ Bayesian_RL/src/baselines/baselines/logger.py
@@ -114,4 +114,31 @@ class CVSOutputFormat(KVWriter):
         self.keys = []
         self.sep = ','
 
+    def writekvs(self, kvs):
+        # Add our current row to the history
+        extra_keys = list(kvs.keys() - self.keys)
+        extra_keys.sort()
+        if extra_keys:
+            self.keys.append(extra_keys)
+            self.file.seek(0)
+            lines = self.file.readlines()
+            self.file.seek(0)
+            for (i, k) in enumerate(self.keys):
+                if i > 0:
+                    self.file.write(',')
+                self.file.write(k)
+            self.file.write('\n')
+            for line in lines[1:]:
+                self.file.write(line[:-1])
+                self.file.write(self.sep * len(extra_keys))
+                self.file.write('\n')
+        for (i, k) in enumerate(self.keys):
+            if i > 0:
+                self.file.write(',')
+            v = kvs.get(k)
+            if v is not None:
+                self.file.write(str(v))
+        self.file.write('\n')
+        self.file.flush()
+    
     
