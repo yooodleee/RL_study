64d311aedb7e8dedac6b04c9a631fbf35689c836
yooodleee <accia25@naver.com>
2025-01-29 21:33:11 +0900

    computes frac of explained variance

--- Bayesian_RL/src/baselines/baselines/common/math_util.py
+++ Bayesian_RL/src/baselines/baselines/common/math_util.py
@@ -23,3 +23,19 @@ def discount(x, gamma):
     return scipy.signal.lfilter([1], [1 - gamma], x[::-1], axis=0)[::-1]
 
 
+def explained_variance(ypred, y):
+    """
+    Computes fraction of variance that ypred explains about y.
+    Returns 1 - Var[y - ypred] / Var[y]
+
+    interpretation:
+        ev = 0  => might as well have predicted zero
+        ev = 1  => perfect prediction
+        ev < 0  => worse than just predicting zero
+
+    """
+    assert y.ndim == 1 and ypred.ndim == 1
+    vary = np.var(y)
+    return np.nan if vary == 0 else 1 - np.var(y - ypred) / vary
+
+
