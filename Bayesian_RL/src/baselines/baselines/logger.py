89e3462c317e0dc7fdd5342df706e95470fae878
yooodleee <accia25@naver.com>
2025-01-29 17:00:29 +0900

    add ProfileKV

--- Bayesian_RL/src/baselines/baselines/logger.py
+++ Bayesian_RL/src/baselines/baselines/logger.py
@@ -289,3 +289,19 @@ record_tabular = logkv()
 dump_tabular = dumpkvs()
 
 
+class ProfileKV:
+    """
+    Usage:
+    with logger.ProfileKV("interesting_scope"):
+        code
+    """
+    def __init__(self, n):
+        self.n = "wait_" + n
+    
+    def __enter__(self):
+        self.t1 = time.time()
+
+    def __exit__(self, type, value, traceback):
+        Logger.CURRENT.name2val[self.n] += time.time() - self.t1
+
+
