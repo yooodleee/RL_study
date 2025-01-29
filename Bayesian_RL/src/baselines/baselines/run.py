0009ff095bf7eceaa0d2cbb4b46c1e491fd7110b
yooodleee <accia25@naver.com>
2025-01-29 19:14:47 +0900

    convert cmd line arg to a dict

--- Bayesian_RL/src/baselines/baselines/run.py
+++ Bayesian_RL/src/baselines/baselines/run.py
@@ -241,3 +241,19 @@ def get_learn_function_defaults(alg, env_type):
     return kwargs
 
 
+def parse_cmdline_kwargs(args):
+    """
+    Convert a list of '='-spaced command-line arguments to a dictionary,
+        evaluating python objects when possible.
+    """
+    def parse(v):
+
+        assert isinstance(v, str)
+        try:
+            return eval(v)
+        except (NameError, SyntaxError):
+            return v
+    
+    return {k: parse(v) for k, v in parse_unknown_args(args).items()}
+
+
