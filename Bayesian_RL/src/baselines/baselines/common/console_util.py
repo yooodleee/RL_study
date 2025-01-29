f30f8fa57f1b820db126e73c1e0029f3ce5b65aa
yooodleee <accia25@naver.com>
2025-01-29 21:05:09 +0900

    color to num

--- Bayesian_RL/src/baselines/baselines/common/console_util.py
+++ Bayesian_RL/src/baselines/baselines/common/console_util.py
@@ -31,3 +31,15 @@ def fmt_item(x, l):
     return " " * (l - len(rep)) + rep
 
 
+color2num = dict(
+    gray=30,
+    red=31,
+    green=32,
+    yellow=33,
+    blue=34,
+    magenta=35,
+    cyan=36,
+    white=37,
+    crimson=38,
+)
+
