a8d8c3a1e3bd871929aa08650cb80c89e8a2b8f9
yooodleee <accia25@naver.com>
2025-01-30 13:16:13 +0900

    get_viewier

--- Bayesian_RL/src/baselines/baselines/common/vec_env/__init__.py
+++ Bayesian_RL/src/baselines/baselines/common/vec_env/__init__.py
@@ -146,4 +146,11 @@ class VecEnv(ABC):
         else:
             return self
     
-    
+    def get_viewer(self):
+        if self.viewer is None:
+            from gym.envs.classic_control import rendering
+            
+            self.viewer = rendering.SimpleImageViewer()
+        return self.viewer
+
+
