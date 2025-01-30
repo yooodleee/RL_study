fa2cc98cb5a356d5f821a25f955da527fbb347e5
yooodleee <accia25@naver.com>
2025-01-30 12:17:13 +0900

    add a boolean flag to parser

--- Bayesian_RL/src/baselines/baselines/common/misc_util.py
+++ Bayesian_RL/src/baselines/baselines/common/misc_util.py
@@ -180,3 +180,33 @@ class RunningAvg(object):
         return self._value
     
 
+def boolean_flag(
+        parser,
+        name,
+        default=False,
+        help=None):
+    
+    """
+    Add a boolean flag to argparse parser.
+
+    Parameters
+    -------------
+    parser: argparser.Parser
+        parser to add the flag to
+    name: str
+        --<name> will enable the flag, while --no-<name> will disable it.
+    default: bool or None
+        default value of the flag.
+    help: str
+        help string for the flag.
+
+    """
+    dest = name.replace('-', '_')
+    parser.add_argument(
+        "--" + name, action="store_true", default=default, dest=dest, help=help
+    )
+    parser.add_argument(
+        "--no-" + name, action="store_false", dest=dest
+    )
+
+
