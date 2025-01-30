4fc1d4b974e39460ef35e04c502da254431d38ef
yooodleee <accia25@naver.com>
2025-01-30 12:36:04 +0900

    unpickle a possible compressed pickle

--- Bayesian_RL/src/baselines/baselines/common/misc_util.py
+++ Bayesian_RL/src/baselines/baselines/common/misc_util.py
@@ -286,3 +286,30 @@ def relatively_safe_pickle_dump(
     os.rename(temp_storage, path)
 
 
+def pickle_load(path, compression=False):
+    """
+    Unpickle a possible compressed pickle.
+
+    Parameters
+    ------------
+    path: str
+        path to the output file.
+    compression: bool
+        if true assumes that pickle was compreesed when created and
+            attempts decompression.
+
+    Returns
+    --------------
+    obj: object
+        the unpickled object.
+
+    """
+    if compression:
+        with zipfile.ZipFile(
+            path, "r", compression=zipfile.ZIP_DEFLATED
+        ) as myzip:
+            with myzip.open("data") as f:
+                return pickle.load(f)
+    else:
+        with open(path, "rb") as f:
+            return pickle.load(f)
