97497f092fc7dc8d590baeca7fb6f813d7b95dfc
yooodleee <accia25@naver.com>
2025-01-30 12:06:48 +0900

    print the num of sec in human readable format

--- Bayesian_RL/src/baselines/baselines/common/misc_util.py
+++ Bayesian_RL/src/baselines/baselines/common/misc_util.py
@@ -87,3 +87,54 @@ def set_global_seeds(i):
     random.seed(myseed)
 
 
+def pretty_eta(seconds_left):
+    """
+    Print the number of seconds in human readable format.
+
+    Examples:
+    2 days
+    2 hours and 37 minutes
+    less than a minute
+
+    Parameters
+    -----------
+    seconds_left: int
+        Number of seconds to be converted to the ETA
+
+    Returns
+    -----------
+    eta: str
+        String representing the pretty ETA.
+
+    """
+    minutes_lefts = seconds_left // 60
+    seconds_left %= 60
+    hours_left = minutes_lefts // 60
+    minutes_lefts %= 60
+    days_left = hours_left // 24
+    hours_left %= 24
+
+
+    def helper(cnt, name):
+        return "{} {}{}".format(str(cnt), name, ('s' if cnt > 1 else ''))
+    
+
+    if days_left > 0:
+        msg = helper(days_left, 'day')
+        if hours_left > 0:
+            msg += ' and ' + helper(hours_left, 'hour')
+        return msg
+    
+    if hours_left > 0:
+        msg = helper(hours_left, 'hour')
+        if minutes_lefts > 0:
+            msg += ' and ' + helper(minutes_lefts, 'minute')
+        return msg
+    
+    if minutes_lefts > 0:
+        return helper(minutes_lefts, 'minute')
+    
+    
+    return 'less than a minute'
+
+
