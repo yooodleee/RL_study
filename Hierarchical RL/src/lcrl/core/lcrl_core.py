948af7bc99d2c87ab22770fff660b5012b60c2e2
yooodleee <accia25@naver.com>
2025-01-31 13:16:09 +0900

    add LCRL

--- Hierarchical RL/src/lcrl/core/lcrl_core.py
+++ Hierarchical RL/src/lcrl/core/lcrl_core.py
@@ -30,5 +30,56 @@ class LCRL:
     
     Methods
     ----------
-    
+    train_ql(number_of_episodes, iteration_threshold, Q_initial_value)
+        employs Episodic Q-learning to synthesise an optimal policy over
+        the product MDP
+    train_nfq(number_of_episodes, iteration_threshold, nfq_replay_buffer_size, num_of_hidden_neurons)
+        employs Episodict Neural Fitted Q-Iteration to synthesise an optimal policy over the product MDP
+    train_ddpg(number_of_episodes, iteration_threshold, ddpg_replay_buffer_size, num_of_hidden_nuerons)
+        employs Episodic Deep Deterministic Policy Gradient to synthesise an optimal policy ober the product MDP
+    reward(automaton_state)
+        shapes the reward function according to the automaton frontier set
+        for more details refer to the tool paper
+    action_space_augmentiation()
+        augments the action space whenever an epsilon transition is expected
+        for more details refer to the tool paper.
+
     """
+
+    def __init__(
+            self,
+            LDBA=None,
+            discount_factor=0.9,
+            learning_rate=0.9,
+            decaying_learning_rate=True,
+            epsilon=0.15):
+        
+        if MDP is None:
+            raise Exception("LCRL expects an MDP object as input")
+
+        self.MDP = MDP
+        if LDBA is None:
+            raise Exception("LCRL expects an LDBA object as input")
+
+        self.LDBA = LDBA
+        self.epsilon_transition_exists = True if LDBA.epsilon_transitions.__len__() > 0 else False
+        self.gamma = discount_factor
+        self.alpha = learning_rate
+
+        self.decay_lr = decaying_learning_rate
+        if self.decay_lr:
+            self.alpha_initial_value = learning_rate
+            self.alpha_final_value = 0.1
+
+        self.epsilon = epsilon
+        self.path_length = []
+        self.Q = {}
+        self.replay_buffers = {}
+        self.Q_initial_value = 0
+        self.early_interruption = 0
+        self.q_at_initial_state = []
+        self.successes_in_test = 0
+        # ###### testing area ####### #
+        self.test = False
+        # ########################### #
+    
