edb41a36e110b0e3ac56857bb8a227545b707492
yooodleee <accia25@naver.com>
2025-02-02 00:26:52 +0900

    add VectorizedSampler

---
+++ METRPO/samplers/vectorized_sampler.py
@@ -0,0 +1,20 @@
+import pickle
+
+import tensorflow as tf
+from samplers.base import BaseSampler
+from envs.parallel_vec_env_executor import ParallelVecEnvExecutor
+from envs.vec_env_executor import VecEnvExecutor
+from rllab.misc import tensor_utils
+import numpy as np
+from rllab.sampler.stateful_pool import ProgBarCounter
+import rllab.misc.logger as logger
+import itertools
+
+
+class VectorizedSampler(BaseSampler):
+
+    def __init__(self, algo, n_envs=None):
+        super(VectorizedSampler, self).__init__(algo)
+        self.n_envs = n_envs
+    
+    
