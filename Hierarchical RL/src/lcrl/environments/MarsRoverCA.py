8e68edfcdfd655933486f32e0a385ff75e6f99bf
yooodleee <accia25@naver.com>
2025-02-06 17:28:21 +0900

    code
    reset and step for stochasticity agent's movement dynamics.

--- Hierarchical RL/src/lcrl/environments/MarsRoverCA.py
+++ Hierarchical RL/src/lcrl/environments/MarsRoverCA.py
@@ -57,4 +57,47 @@ class MarsRover:
         self.action_space = [1, -1]
     
 
+    def reset(self):
+        self.current_state = self.initial_state.copy()
+    
+
+    def step(self, action):
+        """
+        agent movement dynamics:
+            stochasticity
+        """
+
+        traversed_distance = 4 + random.random()
+        noise = np.array(
+            [
+                random.uniform(-0.1, 0.5),
+                random.uniform(-0.1, 0.5)
+            ]
+        )
+        next_state = self.current_state \
+                    + noise \
+                    + np.append(
+                        traversed_distance * np.sin(action[0] * np.pi),
+                        traversed_distance * np.cos(action[0] * np.pi)
+                    )
+        
+        # check for boundary violations
+        if next_state[0] > self.width - 1:
+            next_state[0] = self.width - 1
+        
+        if next_state[1] > self.height - 1:
+            next_state[1] = self.height - 1
+        
+        if next_state[0] < 0:
+            next_state[0] = 0
+        
+        if next_state[1] < 0:
+            next_state[1] = 0
+        
+
+        # update current state
+        self.current_state = next_state
+        return next_state
+    
+
     
