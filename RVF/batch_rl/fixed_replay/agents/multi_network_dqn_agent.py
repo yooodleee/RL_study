0171fa6a4af565f4f0f3ce9a4815803c39ef5911
yooodleee <accia25@naver.com>
2025-02-06 14:18:32 +0900

    init
    add FixedReplayMultiNetworkDQNAgent

---
+++ RVF/batch_rl/fixed_replay/agents/multi_network_dqn_agent.py
@@ -0,0 +1,73 @@
+
+"""
+Multi Network DQN agent with fixed replay buffer(s).
+"""
+
+
+from batch_rl.fixed_replay.replay_memory import fixed_replay_buffer
+from batch_rl.multi_head import multi_network_dqn_agent
+
+
+import gin
+import tensorflow as tf     # compat to version 1.x
+
+
+
+@gin.configurable
+class FixedReplayMultiNetworkDQNAgent(
+    multi_network_dqn_agent.MultiNetworkDQNAgent):
+
+    """
+    MultiNetworkDQNAgent with fixed replay buffer(s).
+    """
+
+    def __init__(
+            self,
+            sess,
+            num_actions,
+            replay_data_dir,
+            replay_suffix=None,
+            **kwargs):
+        
+        """
+        Initializes the agent and constructs the components of its graph.
+
+
+        Args:
+        -------------
+            sess: (tf.Session)
+                for executing ops.
+            num_actions: (int)
+                number of acts the agent can take at any state.
+            replay_data_dir: (str)
+                log dir from which to load the replay buffer.
+            replay_suffix: (int)
+                If not None, then only load the replay buffer corresponding
+                to the specific suffix in data dir.
+            **kwargs:
+                Arbtrary keyword args.
+        """
+
+        assert replay_data_dir is not None
+
+        tf.compat.v1.logging.info(
+            'Creating FixedReplayMultiNetworkDQNAgent with replay directory: %s',
+            replay_data_dir
+        )
+        tf.compat.v1.logging.info(
+            '\t replay_suffix %s',
+            replay_suffix
+        )
+
+        # set replay_log_dir before calling parent's initializer
+        self._replay_data_dir = replay_data_dir
+        self._replay_suffix = replay_suffix
+
+        super(
+            FixedReplayMultiNetworkDQNAgent, self
+        ).__init__(
+            sess, num_actions, **kwargs
+        )
+
+
+    
