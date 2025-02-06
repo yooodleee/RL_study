a1a715324a258ef1b25d5a05c24fa778bd72d943
yooodleee <accia25@naver.com>
2025-02-06 17:48:20 +0900

    step
    agent movement dynamic's are stochasticity.

--- Hierarchical RL/src/lcrl/environments/MarsRoverDA.py
+++ Hierarchical RL/src/lcrl/environments/MarsRoverDA.py
@@ -62,4 +62,67 @@ class MarsRover:
         ]
 
 
+    def reset(self):
+        self.current_state = self.initial_state.copy()
+    
+
+    def step(self, action):
+        """
+        agent movement dynamics:
+            stochasticity
+        """
+
+        traversed_distance = 10 * random.random()
+        noise = np.array(
+            [
+                random.uniform(-0.1, 0.5),
+                random.uniform(-0.1, 0.5)
+            ]
+        )
+
+        if action == 'right':
+            next_state = self.current_state \
+                        + [0, traversed_distance] \
+                        + noise
+        
+        elif action == 'up':
+            next_state = self.current_state \
+                        + [-traversed_distance, 0] \
+                        + noise
+        
+        elif action == 'left':
+            next_state = self.current_state \
+                        + [0, -traversed_distance] \
+                        + noise
+        
+        elif action == 'down':
+            next_state = self.current_state \
+                        + [traversed_distance, 0] \
+                        + noise
+        
+        elif action == 'stay':
+            next_state = self.current_state \
+                        + [0, 0] \
+                        + noise
+        
+
+        # check for boundary violations
+        if next_state[0] > self.width - 1:
+            next_state[0] = self.width - 1
+        
+        if next_state[1] > self.height - 1:
+            next_state[1] = self.height - 1
+        
+        if next_state[0] < 0:
+            next_state[0] = 0
+        
+        if next_state[1] < 0:
+            next_state[1] = 0
+        
+
+        # update current state
+        self.current_state = next_state
+        return next_state
+    
+
     
