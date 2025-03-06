f1fd9cd851d498f8acea594fc7fd84dc697810bd
yooodleee <accia25@naver.com>
2025-03-06 14:49:56 +0900

    code

--- MAML/src/data_generator.py
+++ MAML/src/data_generator.py
@@ -110,8 +110,7 @@ class DataGenerator(object):
         else:
             folders = self.metaval_character_folders
             num_total_batches = 600
-        
-        
+         
         # make list of files
         print('Generating filenames')
         all_filenames = []
@@ -119,3 +118,108 @@ class DataGenerator(object):
             sampled_character_folders = random.sample(folders, self.num_classes)
             random.shuffle(sampled_character_folders)
             labels_and_images = get_images(sampled_character_folders, range(self.num_classes), nb_samples=self.num_samples_per_class, shuffle=False)
+            # make sure the above isn't randomized order
+            labels = [li[0] for li in labels_and_images]
+            filenames = [li[1] for li in labels_and_images]
+            all_filenames.extend(filenames)
+        
+        # make queue for tensorflow to read from
+        filename_queue = tf.compat.v1.train.string_input_producer(tf.convert_to_tensor(all_filenames), shuffle=False)
+        print('Generate image processing ops')
+        image_reader = tf.compat.v1.WholeFileReader()
+        _, image_file = image_reader.read(filename_queue)
+        
+        if FLAGS.datasource == 'miniimagenet':
+            image = tf.image.decode_jpeg(image_file, channels=3)
+            image.set_shape((self.img_size[0], self.img_size[1], 3))
+            image = tf.reshape(image, [self.dim_input])
+            image = tf.cast(image, tf.float32) / 255.0
+        else:
+            image = tf.image.decode_png(image_file)
+            image.set_shape((self.img_size[0], self.img_size[1], 1))
+            image = tf.reshape(image, [self.dim_input])
+            image = tf.cast(image, tf.float32) / 255.0
+            image = 1.0 - image # invert
+        
+        num_preprocess_threads = 1  # TODO - enable this to be set to > 1
+        min_queue_examples = 256
+        examples_per_batch = self.num_classes * self.num_samples_per_class
+        batch_image_size = self.batch_size * examples_per_batch
+        print('Batching images')
+        images = tf.compat.v1.train.batch(
+            [image],
+            batch_size=batch_image_size,
+            num_threads=num_preprocess_threads,
+            capacity=min_queue_examples + 3 * batch_image_size,
+        )
+        all_image_batches, all_label_batches = [], []
+        print('Manipulating image data to be right shape')
+        for i in range(self.batch_size):
+            image_batch = images[i * examples_per_batch:(i + 1) * examples_per_batch]
+
+            if FLAGS.datasource == 'omniglot':
+                # omniglot augments the dataset by rotating digits to create new classes
+                # get rotation class (e.g. 0,1,2,0,0 if there are 5 classes)
+                rotations = tf.compat.v1.multinomial(tf.compat.v1.log([[1., 1., 1., 1.]]), self.num_classes)
+            
+            lable_batch = tf.convert_to_tensor(labels)
+            new_list, new_label_list = [], []
+            for k in range(self.num_samples_per_class):
+                class_idxs = tf.range(0, self.num_classes)
+                class_idxs = tf.compat.v1.random_shuffle(class_idxs)
+
+                true_idxs = class_idxs * self.num_samples_per_class + k
+                new_list.append(tf.gather(image_batch, true_idxs))
+                
+                if FLAGS.datasource == 'omniglot':  # and FLAGS.train:
+                    new_list[-1] = tf.stack(
+                        [
+                            tf.reshape(
+                                tf.image.rot90(
+                                    tf.reshape(new_list[-1][ind], [self.img_size[0], self.img_size[1], 1]),
+                                    k=tf.cast(rotations[0, class_idxs[ind]], tf.int32)), (self.dim_input,))
+                                    for ind in range(self.num_classes
+                            )
+                        ]
+                    )
+                new_label_list.append(tf.gather(lable_batch, true_idxs))
+            
+            new_list = tf.concat(new_list, 0)   # has shape [self.num_classes * self.num_samples_per_class, self.dim_input]
+            new_label_list = tf.concat(new_label_list, 0)
+            all_image_batches.append(new_list)
+            all_label_batches.append(new_label_list)
+        
+        all_image_batches = tf.stack(all_image_batches)
+        all_label_batches = tf.stack(all_label_batches)
+        all_label_batches = tf.one_hot(all_label_batches, self.num_classes)
+
+        return all_image_batches, all_label_batches
+    
+
+    def generate_sinusoid_batch(self, train=True, input_idx=None):
+        """Note train arg is not used (but it is used for omniglot method)
+        input_idx is used during qualitative testing --the num of examples 
+        used for the grad update.
+        """
+
+        amp = np.random.uniform(self.amp_range[0], self.amp_range[1], [self.batch_size])
+        phase = np.random.uniform(self.phase_range[0], self.phase_range[1], [self.batch_size])
+        outputs = np.zeros([self.batch_size, self.num_samples_per_class, self.dim_output])
+        init_inputs = np.zeros([self.batch_size, self.num_samples_per_class, self.dim_input])
+
+        for func in range(self.batch_size):
+            init_inputs[func] = np.random.uniform(
+                self.input_range[0],
+                self.input_range[1],
+                [self.num_samples_per_class, 1],
+            )
+            if input_idx is not None:
+                init_inputs[:, input_idx:, 0]= np.linspace(
+                    self.input_range[0],
+                    self.input_range[1],
+                    num=self.num_samples_per_class - input_idx,
+                    retstep=False,
+                )
+            outputs[func] = amp[func] * np.sin(init_inputs[func] - phase[func])
+        
+        return init_inputs, outputs, amp, phase
---
+++ MAML/src/main.py
@@ -0,0 +1,75 @@
+
+"""Usage Instructions:
+    10-shot sinusoid:
+        python main.py --datasource=sinusoid --logdir=logs/sine/ --metatrain_iterations=70000 --norm=None --update_batch_size=10
+
+    10-shot sinusoid baselines:
+        python main.py --datasource=sinusoid --logdir=logs/sine/ --pretrain_iterations=70000 --metatrain_iterations=0 --norm=None --update_batch_size=10 --baseline=oracle
+        python main.py --datasource=sinusoid --logdir=logs/sine/ --pretrain_iterations=70000 --metatrain_iterations=0 --norm=None --update_batch_size=10
+
+    5-way, 1-shot omniglot:
+        python main.py --datasource=omniglot --metatrain_iterations=60000 --meta_batch_size=32 --update_batch_size=1 --update_lr=0.4 --num_updates=1 --logdir=logs/omniglot5way/
+
+    20-way, 1-shot omniglot:
+        python main.py --datasource=omniglot --metatrain_iterations=60000 --meta_batch_size=16 --update_batch_size=1 --num_classes=20 --update_lr=0.1 --num_updates=5 --logdir=logs/omniglot20way/
+
+    5-way 1-shot mini imagenet:
+        python main.py --datasource=miniimagenet --metatrain_iterations=60000 --meta_batch_size=4 --update_batch_size=1 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=logs/miniimagenet1shot/ --num_filters=32 --max_pool=True
+
+    5-way 5-shot mini imagenet:
+        python main.py --datasource=miniimagenet --metatrain_iterations=60000 --meta_batch_size=4 --update_batch_size=5 --update_lr=0.01 --num_updates=5 --num_classes=5 --logdir=logs/miniimagenet5shot/ --num_filters=32 --max_pool=True
+
+    To run evaluation, use the '--train=False' flag and the '--test_set=True' flag to use the test set.
+
+    For omniglot and miniimagenet training, acquire the dataset online, put it in the correspoding data directory, and see the python script instructions in that directory to preprocess the data.
+
+    Note that better sinusoid results can be achieved by using a larger network.
+
+"""
+
+import csv
+import numpy as np
+import pickle
+import random
+import tensorflow as tf
+
+from data_generator import DataGenerator
+from maml import MAML
+from tensorflow.python.platform import flags
+
+FLAGS = flags.FLAGS
+
+
+## Dataset/method options
+flags.DEFINE_string('datasource', 'sinusoid', 'sinusoid or omniglot or miniimagenet')
+flags.DEFINE_integer('num_classes', 5, 'number of classes used in classification (e.g. 5-way classification).')
+# oracle means task id is input (only suitable for sinusoid)
+flags.DEFINE_string('baseline', None, 'oracle, or None')
+
+## Training ops
+flags.DEFINE_integer('pretrain_iterations', 0, 'number of pre-training iterations.')
+flags.DEFINE_integer('metatrain_iterations', 15000, 'number of metatraining iterations.')  # 15k for omniglot, 50k for sinusoid
+flags.DEFINE_integer('meta_batch_size', 25, 'number of tasks sampled per meta-update')
+flags.DEFINE_integer('meta_lr', 0.001, 'the base learning rate of the generator')
+flags.DEFINE_integer('update_batch_size', 5, 'number of examples used for inner gradient update (K for K-shot learning).')
+flags.DEFINE_integer('update_lr', 1e-3, 'step size alpha for inner gradient update.') # 0.1 for omniglot
+flags.DEFINE_integer('num_updates', 1, 'number of inner gradient updates during training.')
+
+## Model ops
+flags.DEFINE_string('norm', 'batch_norm', 'batch_norm, layer_norm, or None')
+flags.DEFINE_integer('num_filters', 64, 'number of filters for conv nets -- 32 for miniimagenet, 64 for omniglot.')
+flags.DEFINE_bool('conv', True, 'whether or not to use a convolutional network, only applicable in some cases')
+flags.DEFINE_bool('max_pool', False, 'Whether or not to use max pooling rather than strided convolutions')
+flags.DEFINE_bool('stop_grad', False, 'if True, do not use second derivatives in meta-optimization (for speed)')
+
+## Logging saving, and testing ops
+flags.DEFINE_bool('log', True, 'if True, do not log summaries, for debugging code.')
+flags.DEFINE_string('logdir', '/tmp/data', 'directory for summaries and checkpoints.')
+flags.DEFINE_bool('resume', True, 'resume training if there is a model available')
+flags.DEFINE_bool('train', True, 'True to train, False to test.')
+flags.DEFINE_integer('test_iter', -1, 'iteration to load model (-1 for latest model)')
+flags.DEFINE_bool('test_set', False, 'Set to true to test on the test set, False for the validation set.')
+flags.DEFINE_integer('train_update_batch_size', -1, 'number of examples used for gradient update during training (use if you want to test with a different number).')
+flags.DEFINE_float('train_update_lr', -1, 'value of inner gradient step during training. (use if you want to test with a different value)') # 0.1 for omniglot
+
+
